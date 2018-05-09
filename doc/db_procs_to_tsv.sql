239e0441ef7349fbc545e6c699996a84df3b15d6
Karl Levik <karl.levik@diamond.ac.uk>
2018-05-09 22:27:15 +0100

    Bold proc names

---
+++ doc/db_procs_to_tsv.sql
@@ -0,0 +1,15 @@
+SELECT concat('**', r.ROUTINE_NAME, '**') as "name",
+    concat('"',trim(REPLACE(REPLACE(REPLACE(p.param_list, '\n', ' '), '\r', ' '), '\t', ' ')), '"') as "params",
+    concat('"', r.ROUTINE_COMMENT, '"') as "comment",
+    r.LAST_ALTERED as "modified"
+--    ,GROUP_CONCAT(pp.user) as "users"
+FROM information_schema.ROUTINES r
+--    LEFT OUTER JOIN mysql.procs_priv pp ON pp.routine_name = r.routine_name
+    INNER JOIN mysql.proc p ON p.name = r.routine_name AND p.db = r.ROUTINE_SCHEMA
+WHERE r.ROUTINE_SCHEMA = database()
+    AND r.ROUTINE_TYPE = 'PROCEDURE'
+-- GROUP BY r.ROUTINE_NAME,
+--    r.ROUTINE_COMMENT,
+--    r.LAST_ALTERED,
+--    TRIM(REPLACE(REPLACE(REPLACE(p.param_list, '\n', ' '), '\r', ' '), '\t', ' '))
+ORDER BY ROUTINE_NAME ASC;
