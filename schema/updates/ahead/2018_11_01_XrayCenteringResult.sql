e2e8504c127102d9e98f0f6f8fbc92703ea4163f
Karl Levik <karl.levik@diamond.ac.uk>
2018-11-01 14:10:30 +0000

    Add SchemaStatus entries

--- schema/updates/ahead/2018_11_01_XrayCenteringResult.sql
+++ schema/updates/ahead/2018_11_01_XrayCenteringResult.sql
@@ -1,3 +1,5 @@
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2018_11_01_XrayCenteringResult.sql', 'ONGOING');
+
 CREATE TABLE XrayCenteringResult (
     xrayCenteringResultId int(11) unsigned auto_increment PRIMARY KEY,
     gridInfoId int(11) unsigned NOT NULL,
@@ -7,3 +9,5 @@ CREATE TABLE XrayCenteringResult (
     y float COMMENT 'position in number of boxes in direction of the slow scan within GridInfo grid',
     CONSTRAINT XrayCenteringResult_ibfk_1 FOREIGN KEY (gridInfoId) REFERENCES GridInfo(gridInfoId) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2018_11_01_XrayCenteringResult.sql';
