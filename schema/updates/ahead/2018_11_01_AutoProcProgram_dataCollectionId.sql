73eb1766291b5bb13f10e4fbb060a0588eab1a18
Karl Levik <karl.levik@diamond.ac.uk>
2018-11-01 15:52:29 +0000

    Add warning comment

--- schema/updates/ahead/2018_11_01_AutoProcProgram_dataCollectionId.sql
+++ schema/updates/ahead/2018_11_01_AutoProcProgram_dataCollectionId.sql
@@ -1,5 +1,7 @@
 INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2018_11_01_AutoProcProgram_dataCollectionId.sql', 'ONGOING');
 
+-- Warning: This will take a while to complete at least on MariaDB 10.2 
+
 SET foreign_key_checks=OFF;
 ALTER TABLE AutoProcProgram
   ADD dataCollectionId int(11) unsigned,
