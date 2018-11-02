b93f9738634f76aef66287523ad68774ea008ec4
Karl Levik <karl.levik@diamond.ac.uk>
2018-11-02 11:54:37 +0000

    Add SchemaStatus entry

--- schema/updates/ahead/2018_11_02_DataCollectionGroup_experimentType_enum.sql
+++ schema/updates/ahead/2018_11_02_DataCollectionGroup_experimentType_enum.sql
@@ -1,1 +1,5 @@
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2018_11_02_DataCollectionGroup_experimentType_enum.sql', 'ONGOING');
+
 ALTER TABLE DataCollectionGroup MODIFY experimentType enum('SAD','SAD - Inverse Beam','OSC','Collect - Multiwedge','MAD','Helical','Multi-positional','Mesh','Burn','MAD - Inverse Beam','Characterization','Dehydration','tomo','experiment','EM','PDF','PDF+Bragg','Bragg','single particle', 'Serial Fixed', 'Serial Jet', 'Standard', 'Time Resolved', 'Diamond Anvil High Pressure', 'Custom');
+
+UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2018_11_02_DataCollectionGroup_experimentType_enum.sql';
