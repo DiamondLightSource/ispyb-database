87b3a61ebdf9b6b7e888f83d13213d0e665c9b61
Karl Levik <karl.levik@diamond.ac.uk>
2019-01-14 12:33:43 +0000

    Fix issue #1: tables.sql has tables with AUTO_INCREMENT=some number

--- bin/backup.sh
+++ bin/backup.sh
@@ -8,10 +8,11 @@
 OUT_DIR=$1
 HOST=localhost
 DB=ispyb_build
+OPTIONS="--add-drop-table --add-locks --create-options --disable-keys --extended-insert --lock-tables --quick --set-charset --single-transaction --max_allowed_packet=1G --host=${HOST} --port=3306 --default-character-set=utf8"
 
-mysqldump --defaults-file=../.my.cnf --opt --single-transaction --max_allowed_packet=1G --host=${HOST} --port=3306 --default-character-set=utf8 --skip-triggers --no-data "${DB}" | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' | sed -e 's/CREATE .* PROCEDURE/CREATE PROCEDURE/' | sed -e 's/CREATE .* FUNCTION/CREATE FUNCTION/' > ${OUT_DIR}/tables.sql
+mysqldump --defaults-file=../.my.cnf ${OPTIONS} --skip-triggers --no-data "${DB}" | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' | sed 's/ AUTO_INCREMENT=[0-9]*\b//g' > ${OUT_DIR}/tables.sql
 
-mysqldump --defaults-file=../.my.cnf --opt --single-transaction --max_allowed_packet=1G --host=${HOST} --port=3306 --default-character-set=utf8 --skip-triggers --routines --no-create-info --no-data "${DB}" | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' | sed -e 's/CREATE .* PROCEDURE/CREATE PROCEDURE/' | sed -e 's/CREATE .* FUNCTION/CREATE FUNCTION/' > ${OUT_DIR}/routines.sql
+mysqldump --defaults-file=../.my.cnf ${OPTIONS} --skip-triggers --routines --no-create-info --no-data "${DB}" | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' | sed -e 's/CREATE .* PROCEDURE/CREATE PROCEDURE/' | sed -e 's/CREATE .* FUNCTION/CREATE FUNCTION/' > ${OUT_DIR}/routines.sql
 
 LOOKUP_TABLES=(
 AdminVar
@@ -39,7 +40,7 @@ do :
     LOOKUP_TABLES_STRING+=" ${TABLE}"
 done
 
-mysqldump --defaults-file=../.my.cnf --opt --single-transaction --max_allowed_packet=1G --host=${HOST} --port=3306 --default-character-set=utf8 --skip-triggers --no-create-info --complete-insert ${DB} ${LOOKUP_TABLES_STRING} | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' | sed -e 's/CREATE .* PROCEDURE/CREATE PROCEDURE/' | sed -e 's/CREATE .* FUNCTION/CREATE FUNCTION/' > ${OUT_DIR}/lookups.sql
+mysqldump --defaults-file=../.my.cnf ${OPTIONS} --skip-triggers --no-create-info --complete-insert ${DB} ${LOOKUP_TABLES_STRING} | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' > ${OUT_DIR}/lookups.sql
 
 IGNORED_TABLES_STRING=''
 for TABLE in "${LOOKUP_TABLES[@]}"
@@ -47,4 +48,4 @@ do :
     IGNORED_TABLES_STRING+=" --ignore-table=${DB}.${TABLE}"
 done
 
-mysqldump --defaults-file=../.my.cnf --opt --single-transaction --max_allowed_packet=1G --host=${HOST} --port=3306 --default-character-set=utf8 --skip-triggers --no-create-info --complete-insert ${IGNORED_TABLES_STRING} ${DB} | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' | sed -e 's/CREATE .* PROCEDURE/CREATE PROCEDURE/' | sed -e 's/CREATE .* FUNCTION/CREATE FUNCTION/' > ${OUT_DIR}/data.sql
+mysqldump --defaults-file=../.my.cnf ${OPTIONS} --skip-triggers --no-create-info --complete-insert ${IGNORED_TABLES_STRING} ${DB} | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' > ${OUT_DIR}/data.sql
