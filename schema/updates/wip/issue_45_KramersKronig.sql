5980c91ad42a17ee8866e3f7ec30f8fb95cfb4d4
Karl Levik <KarlLevik@users.noreply.github.com>
2019-10-17 14:36:03 +0100

    Format, fixes, tweaks

--- schema/updates/wip/issue_45_KramersKronig.sql
+++ schema/updates/wip/issue_45_KramersKronig.sql
@@ -3,15 +3,16 @@
 -- https://github.com/ispyb/ispyb-database-modeling/issues/45
 
 CREATE TABLE KramersKronig (
-  kramersKronigId
-  autoProcProgramId
-  edgeEnergy
-  peakEnergy
-  inflectionEnergy
-  inflectionFDoublePrime
-  inflectionFPrime
-  peakFDoublePrime
-  peakFPrime
-  fitFileFullPath varchar(255) COMMENT 'Full path for e.g. Chooch file'
-  jpegFitFileFullPath varchar(255) COMMENT 'Full path for e.g. Chooch jpeg file'
+  kramersKronigId int(11) unsigned auto_increment PRIMARY KEY,
+  autoProcProgramId int(11) unsigned,
+  edgeEnergy varchar(3),
+  peakEnergy float,
+  inflectionEnergy float,
+  inflectionFDoublePrime float,
+  inflectionFPrime float,
+  peakFDoublePrime float,
+  peakFPrime float,
+  fitFileFullPath varchar(255) COMMENT 'Full path for e.g. Chooch file',
+  jpegFitFileFullPath varchar(255) COMMENT 'Full path for e.g. Chooch jpeg file',
+  CONSTRAINT KramersKronig_fk1 FOREIGN KEY (autoProcProgramId) REFERENCES AutoProcProgram(autoProcProgramId) ON DELETE NO ACTION ON UPDATE CASCADE
 );
--- schema/updates/wip/issue_45_MCAProcessing.sql
+++ schema/updates/wip/issue_45_MCAProcessing.sql
@@ -2,20 +2,22 @@
 -- See issue description at:
 -- https://github.com/ispyb/ispyb-database-modeling/issues/45
 
-CREATE TABLE MCAProcessing
-  mcaprocessingid
-  autoprocprogramid
-  annotatedSpectrum
-  fittedDataFileFullPath
-  peakListFullPath
+CREATE TABLE MCAProcessing (
+  mcaProcessingId int(11) unsigned auto_increment PRIMARY KEY,
+  autoProcProgramId int(10) unsigned NOT NULL,
+  annotatedSpectrum varchar(255),
+  fittedDataFileFullPath varchar(255),
+  peakListFullPath varchar(255),
+  CONSTRAINT MCAProcessing_fk1 FOREIGN KEY (autoProcProgramId) REFERENCES AutoProcProgram(autoProcProgramId) ON DELETE NO ACTION ON UPDATE CASCADE
 )
 COMMENT 'Element analysis';
 
 CREATE TABLE MCAProcessingPeaks (
-  mcaprocessingpeakid
-  mcaprocessingid
-  name
-  energy
-  intensity
+  mcaProcessingPeakId int(11) unsigned auto_increment PRIMARY KEY,
+  mcaProcessingId int(11) unsigned,
+  name varchar(6),
+  energy float,
+  intensity float,
+  CONSTRAINT MCAProcessingPeaks_fk1 FOREIGN KEY (mcaProcessingId) REFERENCES MCAProcessing(mcaProcessingId) ON DELETE CASCADE ON UPDATE CASCADE
 )
-COMMENT '(this will allow me to construct the annotated spectrum in synchweb from the datacollection scanfile + mcapeak list)';
+COMMENT 'This enables us to construct the annotated spectrum from the DataCollection scanfile + mcapeak list';
