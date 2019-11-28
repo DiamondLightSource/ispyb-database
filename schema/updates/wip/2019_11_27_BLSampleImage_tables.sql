e5d2e2d041b8a17935f4f0d9ac0f2fb5a43a8b17
Karl Levik <KarlLevik@users.noreply.github.com>
2019-11-28 10:31:06 +0000

    Make BLSampleImageAutoScoreClass child of BLSampleImageAutoScoreSchema

--- schema/updates/wip/2019_11_27_BLSampleImage_tables.sql
+++ schema/updates/wip/2019_11_27_BLSampleImage_tables.sql
@@ -2,8 +2,8 @@ INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2019_11_27_B
 
 CREATE TABLE BLSampleImageAutoScoreSchema (
   blSampleImageAutoScoreSchemaId tinyint(3) unsigned auto_increment PRIMARY KEY,
-  schemaName varchar(25) COMMENT 'Name of the schema e.g. Hampton, MARCO',
-  enabled tinyint(1) COMMENT 'Whether this schema is enabled (could be configurable in the UI)'
+  schemaName varchar(25) NOT NULL COMMENT 'Name of the schema e.g. Hampton, MARCO',
+  enabled tinyint(1) DEFAULT 1 COMMENT 'Whether this schema is enabled (could be configurable in the UI)'
 );
 
 INSERT INTO BLSampleImageAutoScoreSchema (
@@ -12,27 +12,26 @@ INSERT INTO BLSampleImageAutoScoreSchema (
 
 CREATE TABLE BLSampleImageAutoScoreClass (
   blSampleImageAutoScoreClassId tinyint(3) unsigned auto_increment PRIMARY KEY,
-  scoreClass varchar(15) COMMENT 'Thing being scored e.g. crystal, precipitant'
+  blSampleImageAutoScoreSchemaId tinyint(3) unsigned,
+  scoreClass varchar(15) NOT NULL COMMENT 'Thing being scored e.g. crystal, precipitant',
+  CONSTRAINT BLSampleImageAutoScoreClass_fk1 FOREIGN KEY (blSampleImageAutoScoreSchemaId) REFERENCES BLSampleImageAutoScoreSchema(blSampleImageAutoScoreSchemaId) ON DELETE NO ACTION ON UPDATE CASCADE
 ) COMMENT 'The automated scoring classes';
 
 INSERT INTO BLSampleImageAutoScoreClass (
-  blSampleImageAutoScoreClassId, scoreClass)
+  blSampleImageAutoScoreClassId, blSampleImageAutoScoreSchemaId, scoreClass)
 VALUES
-  (1, 'clear'),
-  (2, 'crystal'),
-  (3, 'precipitant'),
-  (4, 'other');
+  (1, 1, 'clear'),
+  (2, 1, 'crystal'),
+  (3, 1, 'precipitant'),
+  (4, 1, 'other');
 
-CREATE TABLE BLSampleImage_has_AutoScore (
-  blSampleImageId int(11) unsigned,
-  blSampleImageAutoScoreSchemaId tinyint(3) unsigned,
+CREATE TABLE BLSampleImage_has_AutoScoreClass (
+  blSampleImageId int(11) unsigned NOT NULL,
   blSampleImageAutoScoreClassId tinyint(3) unsigned,
   probability float,
-  PRIMARY KEY (blSampleImageId, blSampleImageAutoScoreSchemaId, blSampleImageAutoScoreClassId),
-  CONSTRAINT BLSampleImage_has_AutoScore_fk1 FOREIGN KEY (blSampleImageId) REFERENCES BLSampleImage(blSampleImageId) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT BLSampleImage_has_AutoScore_fk2 FOREIGN KEY (blSampleImageAutoScoreSchemaId) REFERENCES BLSampleImageAutoScoreSchema(blSampleImageAutoScoreSchemaId) ON DELETE NO ACTION ON UPDATE CASCADE,
-  CONSTRAINT BLSampleImage_has_AutoScore_fk3 FOREIGN KEY (blSampleImageAutoScoreClassId) REFERENCES BLSampleImageAutoScoreClass(blSampleImageAutoScoreClassId) ON DELETE NO ACTION ON UPDATE CASCADE
+  PRIMARY KEY (blSampleImageId, blSampleImageAutoScoreClassId),
+  CONSTRAINT BLSampleImage_has_AutoScoreClass_fk1 FOREIGN KEY (blSampleImageId) REFERENCES BLSampleImage(blSampleImageId) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT BLSampleImage_has_AutoScoreClass_fk2 FOREIGN KEY (blSampleImageAutoScoreClassId) REFERENCES BLSampleImageAutoScoreClass(blSampleImageAutoScoreClassId) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2019_11_27_BLSampleImage_tables.sql';
