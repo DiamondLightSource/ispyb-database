5350d6525a4169aa292b621ccab36f64c63c5a98
Karl Levik <KarlLevik@users.noreply.github.com>
2020-01-07 10:00:04 +0000

    Rename+rewrite SP for writing sample image auto scores + bump version

--- schema/stored_programs/sp_upsert_sample_image_auto_score.sql
+++ schema/stored_programs/sp_upsert_sample_image_auto_score.sql
@@ -1,13 +1,12 @@
 DELIMITER ;;
-CREATE OR REPLACE PROCEDURE `insert_sample_image_auto_score`(
-     OUT p_id int(11) unsigned,
+CREATE OR REPLACE PROCEDURE `upsert_sample_image_auto_score`(
      p_imageFullPath varchar(255),
      p_schemaName varchar(25),
      p_scoreClass varchar(15),
      p_probability float
 )
 MODIFIES SQL DATA
-COMMENT 'Insert a row with the auto scored probability for a given sample image using a certain class and schema. Returns the ID in p_id.'
+COMMENT 'Insert or update a row with the auto scored probability for a given sample image with a certain class and schema. Returns nothing.'
 BEGIN
     DECLARE l_blSampleImageId int(11) unsigned;
     DECLARE l_blSampleImageAutoScoreClassId tinyint(3) unsigned;
@@ -20,10 +19,12 @@ BEGIN
         WHERE bsiasc.scoreClass = p_scoreClass AND bsiass.schemaName = p_schemaName AND bsiass.enabled > 0;
 
         INSERT INTO BLSampleImage_has_AutoScoreClass (blSampleImageId, blSampleImageAutoScoreClassId, probability)
-          VALUES (l_blSampleImageId, l_blSampleImageAutoScoreClassId, p_probability);
-
-        SET p_id = LAST_INSERT_ID();
-
+          VALUES (l_blSampleImageId, l_blSampleImageAutoScoreClassId, p_probability) ON DUPLICATE KEY UPDATE
+              blSampleImageId = IFNULL(l_blSampleImageId, blSampleImageId),
+              blSampleImageAutoScoreClassId = IFNULL(l_blSampleImageAutoScoreClassId, blSampleImageAutoScoreClassId),
+              probability = IFNULL(p_probability, probability);
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MYSQL_ERRNO=1644, MESSAGE_TEXT='Arguments p_imageFullPath, p_schemaName and p_scoreClass cannot be NULL';
 	  END IF;
 END;;
 DELIMITER ;
