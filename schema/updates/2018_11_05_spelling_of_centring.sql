f883046886164b1dd74f2782bcf38c0ad8782816
Karl Levik <KarlLevik@users.noreply.github.com>
2020-02-17 16:24:59 +0000

    Missing semi-colon

--- schema/updates/2018_11_05_spelling_of_centring.sql
+++ schema/updates/2018_11_05_spelling_of_centring.sql
@@ -3,6 +3,6 @@ INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2018_11_05_spelling
 RENAME TABLE XrayCenteringResult TO XrayCentringResult;
 ALTER TABLE XrayCentringResult CHANGE xrayCenteringResultId xrayCentringResultId int(11) unsigned NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE DiffractionPlan CHANGE centeringMethod centringMethod enum('xray','loop','diffraction','optical') DEFAULT NULL
+ALTER TABLE DiffractionPlan CHANGE centeringMethod centringMethod enum('xray','loop','diffraction','optical') DEFAULT NULL;
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2018_11_05_spelling_of_centring.sql';
