db329984a216198de2e7b020188df47930012976
Karl Levik <KarlLevik@users.noreply.github.com>
2020-03-25 10:18:56 +0000

    If p_run is null, use current run

--- schema/stored_programs/sp_retrieve_sessions_for_beamline_and_run.sql
+++ schema/stored_programs/sp_retrieve_sessions_for_beamline_and_run.sql
@@ -4,18 +4,37 @@ CREATE OR REPLACE DEFINER=`ispyb_root`@`%` PROCEDURE `retrieve_sessions_for_beam
   IN p_run varchar(7)
 )
 READS SQL DATA
-COMMENT 'Returns a multi-row result-set with the sessions (mx12345-123), their start and end dates for beamline p_beamline and run p_run'
+COMMENT 'Returns a multi-row result-set with the sessions (mx12345-123), their start and end dates for beamline p_beamline and run p_run. If called with NULL for p_run, then use the current run.'
 BEGIN
-  SELECT
-      bs.sessionId "sessionId",
-      concat(p.proposalCode, p.proposalNumber, '-', bs.visit_number) "session",
-      bs.startDate "startDate",
-      bs.endDate "endDate"
-  FROM Proposal p
-      INNER JOIN BLSession bs on p.proposalId = bs.proposalId
-      INNER JOIN v_run vr ON bs.startDate BETWEEN vr.startDate AND vr.endDate
-	WHERE bs.beamlinename = p_beamline AND vr.run = p_run
-  ORDER BY bs.startDate, bs.sessionId;
+  IF p_run IS NULL THEN
+
+    SELECT
+        bs.sessionId "sessionId",
+        concat(p.proposalCode, p.proposalNumber, '-', bs.visit_number) "session",
+        bs.startDate "startDate",
+        bs.endDate "endDate"
+    FROM Proposal p
+        INNER JOIN BLSession bs on p.proposalId = bs.proposalId
+        INNER JOIN v_run vr ON bs.startDate BETWEEN vr.startDate AND vr.endDate
+	  WHERE bs.beamlinename = p_beamline
+        AND bs.visit_number <> 0
+        AND now() BETWEEN vr.startDate AND vr.endDate
+    ORDER BY bs.startDate, bs.sessionId;
+
+  ELSE
+    SELECT
+        bs.sessionId "sessionId",
+        concat(p.proposalCode, p.proposalNumber, '-', bs.visit_number) "session",
+        bs.startDate "startDate",
+        bs.endDate "endDate"
+    FROM Proposal p
+        INNER JOIN BLSession bs on p.proposalId = bs.proposalId
+        INNER JOIN v_run vr ON bs.startDate BETWEEN vr.startDate AND vr.endDate
+  	WHERE bs.beamlinename = p_beamline
+        AND bs.visit_number <> 0
+        AND vr.run = p_run
+    ORDER BY bs.startDate, bs.sessionId;
+  END IF;
 END;;
 
 DELIMITER ;
