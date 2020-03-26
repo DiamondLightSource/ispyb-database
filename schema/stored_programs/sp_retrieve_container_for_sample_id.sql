0badb43f2f3df13ac429ca935307626406e8a140
Karl Levik <KarlLevik@users.noreply.github.com>
2020-03-26 16:29:14 +0000

    Fix formatting, col. aliases

--- schema/stored_programs/sp_retrieve_container_for_sample_id.sql
+++ schema/stored_programs/sp_retrieve_container_for_sample_id.sql
@@ -5,47 +5,70 @@ COMMENT 'Returns a single-row result-set with the container and its processing p
 BEGIN
     IF p_id IS NOT NULL THEN
 
-    	IF p_authLogin IS NOT NULL THEN
-    	-- Authorise only if the person (p_authLogin) is a member of a session on the proposal.
+      IF p_authLogin IS NOT NULL THEN
+      -- Authorise only if the person (p_authLogin) is a member of a session on
+      -- the proposal.
 
-				SELECT c.dewarId, c.code, c.containerType, c.capacity, c.sampleChangerLocation, c.containerStatus, c.bltimeStamp, c.beamlineLocation, c.screenId, c.scheduleId, c.barcode, c.imagerId, c.sessionId, c.ownerId, c.requestedImagerId, c.requestedReturn, c.comments, c.experimentType, c.storageTemperature, c.containerRegistryId,
-        cr.barcode "containerRegistryBarcode", cr.comments "containerRegistryComments", cr.recordTimestamp "containerRegistryRecordTimestamp",
-        o.familyName "ownerFamilyName", o.givenName "ownerGivenName", o.emailAddress "ownerEmailAddress",
-        pp.name "processingPipelineName", pp.discipline "processingPipelineDiscipline", pp.pipelineStatus "processingPipelinePipelineStatus", pp.reprocessing "processingPipelineReprocessing",
-        ppc.name "processingPipelineCategoryName"
-				FROM BLSample bls
-          INNER JOIN Container c USING (containerId)
-          INNER JOIN Dewar d USING (dewarId)
-          INNER JOIN Shipping s USING(shippingId)
-          INNER JOIN BLSession bs ON bs.proposalId = s.proposalId
-          INNER JOIN Session_has_Person shp ON shp.sessionId = bs.sessionId
-          INNER JOIN Person p ON p.personId = shp.personId
-          LEFT OUTER JOIN Person o ON o.personId = c.ownerId
-          LEFT OUTER JOIN ContainerRegistry cr USING(containerRegistryId)
-          LEFT OUTER JOIN ProcessingPipeline pp ON pp.processingPipelineId = c.priorityPipelineId
-          LEFT OUTER JOIN ProcessingPipelineCategory ppc USING(processingPipelineCategoryId)
-				WHERE bls.blSampleId = p_id
-	  		  AND p.login = p_authLogin;
-
-    	ELSE
-
-				SELECT c.dewarId, c.code, c.containerType, c.capacity, c.sampleChangerLocation, c.containerStatus, c.bltimeStamp, c.beamlineLocation, c.screenId, c.scheduleId, c.barcode, c.imagerId, c.sessionId, c.ownerId, c.requestedImagerId, c.requestedReturn, c.comments, c.experimentType, c.storageTemperature, c.containerRegistryId,
-        cr.barcode "containerRegistryBarcode", cr.comments "containerRegistryComments", cr.recordTimestamp "containerRegistryRecordTimestamp",
-        o.familyName "ownerFamilyName", o.givenName "ownerGivenName", o.emailAddress "ownerEmailAddress",
-        pp.name "processingPipelineName", pp.discipline "processingPipelineDiscipline", pp.pipelineStatus "processingPipelinePipelineStatus", pp.reprocessing "processingPipelineReprocessing",
-        ppc.name "processingPipelineCategoryName"
-				FROM BLSample bls
+        SELECT c.dewarId, c.code, c.containerType, c.capacity,
+          c.sampleChangerLocation, c.containerStatus, c.bltimeStamp
+          "blTimeStamp", c.beamlineLocation, c.screenId, c.scheduleId,
+          c.barcode, c.imagerId, c.sessionId, c.ownerId, c.requestedImagerId,
+          c.requestedReturn, c.comments, c.experimentType,
+          c.storageTemperature, c.containerRegistryId,
+          cr.barcode "containerRegistryBarcode", cr.comments
+          "containerRegistryComments", cr.recordTimestamp
+          "containerRegistryRecordTimestamp",
+          o.familyName "ownerFamilyName", o.givenName "ownerGivenName",
+          o.emailAddress "ownerEmailAddress",
+          pp.name "processingPipelineName", pp.discipline
+          "processingPipelineDiscipline", pp.pipelineStatus
+          "processingPipelinePipelineStatus", pp.reprocessing
+          "processingPipelineReprocessing",
+          ppc.name "processingPipelineCategoryName"
+        FROM BLSample bls
           INNER JOIN Container c USING (containerId)
           LEFT OUTER JOIN Person o ON o.personId = c.ownerId
           LEFT OUTER JOIN ContainerRegistry cr USING(containerRegistryId)
-          LEFT OUTER JOIN ProcessingPipeline pp ON pp.processingPipelineId = c.priorityPipelineId
-          LEFT OUTER JOIN ProcessingPipelineCategory ppc USING(processingPipelineCategoryId)
-				WHERE bls.blSampleId = p_id;
+          LEFT OUTER JOIN ProcessingPipeline pp
+            ON pp.processingPipelineId = c.priorityPipelineId
+          LEFT OUTER JOIN ProcessingPipelineCategory ppc
+            USING(processingPipelineCategoryId)
+        WHERE bls.blSampleId = p_id
+          AND p.login = p_authLogin;
+
+      ELSE
+
+        SELECT c.dewarId, c.code, c.containerType, c.capacity,
+          c.sampleChangerLocation, c.containerStatus, c.bltimeStamp
+          "blTimeStamp", c.beamlineLocation, c.screenId, c.scheduleId,
+          c.barcode, c.imagerId, c.sessionId, c.ownerId, c.requestedImagerId,
+          c.requestedReturn, c.comments, c.experimentType,
+          c.storageTemperature, c.containerRegistryId,
+          cr.barcode "containerRegistryBarcode", cr.comments
+          "containerRegistryComments", cr.recordTimestamp
+          "containerRegistryRecordTimestamp",
+          o.familyName "ownerFamilyName", o.givenName "ownerGivenName",
+          o.emailAddress "ownerEmailAddress",
+          pp.name "processingPipelineName", pp.discipline
+          "processingPipelineDiscipline", pp.pipelineStatus
+          "processingPipelinePipelineStatus", pp.reprocessing
+          "processingPipelineReprocessing",
+          ppc.name "processingPipelineCategoryName"
+        FROM BLSample bls
+          INNER JOIN Container c USING (containerId)
+          LEFT OUTER JOIN Person o ON o.personId = c.ownerId
+          LEFT OUTER JOIN ContainerRegistry cr USING(containerRegistryId)
+          LEFT OUTER JOIN ProcessingPipeline pp
+            ON pp.processingPipelineId = c.priorityPipelineId
+          LEFT OUTER JOIN ProcessingPipelineCategory ppc
+            USING(processingPipelineCategoryId)
+        WHERE bls.blSampleId = p_id;
 
     	END IF;
 
     ELSE
-	  SIGNAL SQLSTATE '45000' SET MYSQL_ERRNO=1644, MESSAGE_TEXT='Mandatory arguments p_id can not be NULL';
-	END IF;
+      SIGNAL SQLSTATE '45000' SET MYSQL_ERRNO=1644,
+        MESSAGE_TEXT='Mandatory arguments p_id can not be NULL';
+  END IF;
 END;;
 DELIMITER ;
