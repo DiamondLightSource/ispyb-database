dce3f53b7b11d3e814e80d35cb0b2196b1cb5b1c
Karl Levik <KarlLevik@users.noreply.github.com>
2020-04-23 15:41:08 +0100

    Scripts to remove PII for Persons incl. info in LabContact

---
+++ bin/pii_forget_all_persons.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Caution: This script attempts to remove all Personally Identifiable
+# Information (PII) in the given ispyb database for the given proposal
+# and session.
+#
+# Usage:
+# pii_forget_all_persons.sh <.cnf file> <db_name> <proposalCode><proposalNumber> <session number>
+#
+# Usage example:
+# ./pii_forget_all_persons.sh ~/.my.cnf ispyb cm12345 2
+# Author: Karl Levik
+# Date: 2020-03-09
+
+HOST=localhost
+MYCNF=$1
+DB=$2
+PROPOSAL=$3
+SESSNUM=$4
+
+# Get the proposal PID and the session SID:
+
+PID=`mysql --defaults-file=${MYCNF} -s -D ${DB} -e "SELECT proposalId FROM Proposal WHERE concat(proposalCode, proposalNumber)='${PROPOSAL}';"`
+SID=`mysql --defaults-file=${MYCNF} -s -D ${DB} -e "SELECT sessionId FROM BLSession WHERE proposalId='${PID}' AND visit_number=${SESSNUM};"`
+
+# Get all personIds related to the PID and SID
+PERSIDS=`mysql --defaults-file=${MYCNF} -D ${DB} --skip-column-names --silent --raw -e "SELECT personId FROM Proposal WHERE proposalId=${PID} UNION SELECT personId FROM Person WHERE personId IN (SELECT personId FROM LabContact WHERE proposalId=${PID})   OR   personId IN (SELECT personId FROM Session_has_Person WHERE sessionId=${SID})   OR   personId IN (SELECT personId FROM ProposalHasPerson WHERE proposalId=${PID})   OR   personId IN (SELECT ownerId FROM Container WHERE sessionId=${SID})   OR   personId IN (SELECT c.ownerId FROM Container c INNER JOIN Dewar d USING(dewarId) INNER JOIN Shipping s USING(shippingId) WHERE s.proposalId=${PID})   OR   personId IN (SELECT c.ownerId FROM Container c INNER JOIN BLSample bls USING(containerId) INNER JOIN Crystal USING(crystalId) INNER JOIN Protein p USING(proteinId) WHERE p.proposalId=${PID})   OR   personId IN (SELECT s.deliveryAgent_flightCodePersonId FROM Shipping s WHERE s.proposalId=${PID});"`
+
+# iterate over and forget:
+
+while read -r PERSID; do
+  `./pii_forget_person.sh ${MYCNF} ${DB} ${PERSID}`
+done <<< "${PERSIDS}"
---
+++ bin/pii_forget_person.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This script removes Personally Identifiable Information (PII) for a particular person.
+# Usage:
+# pii_forget_person.sh <.cnf file> <db_name> <personId>
+# Usage example:
+# pii_forget_person.sh ~/.my.cnf ispyb 21414
+# Author: Karl Levik
+# Date: 2020-03-04
+
+HOST=localhost
+MYCNF=$1
+DB=$2
+PERSONID=$3
+
+mysql --defaults-file=${MYCNF} -s -D ${DB} -e "UPDATE Person SET familyName='Doe', givenName='John', title=NULL, emailAddress='hello@example.com', phoneNumber=NULL, login='john-${PERSONID}', faxNumber=NULL, cache=NULL, externalId=NULL WHERE personId='${PERSONID}';"
+mysql --defaults-file=${MYCNF} -s -D ${DB} -e "UPDATE LabContact SET cardName=concat('Doe', labContactId), defaultCourrierCompany='Doe', courierAccount='12345', billingReference=NULL, dewarAvgCustomsValue=0, dewarAvgTransportValue=0 WHERE personId='${PERSONID}';"
