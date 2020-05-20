ca309c196d0f4785a70944d0536b9dc7183f6808
Karl Levik <KarlLevik@users.noreply.github.com>
2020-05-20 16:29:52 +0100

    rm unnecessary quotes

--- bin/forget_protein.sh
+++ bin/forget_protein.sh
@@ -16,4 +16,4 @@ MYCNF=$1
 DB=$2
 PROTEINID=$3
 
-mysql --defaults-file=${MYCNF} -s -D ${DB} -e "UPDATE Protein SET name='prot-${PROTEINID}', acronym='prot-${PROTEINID}', safetyLevel='GREEN', molecularMass=NULL, proteinType=NULL, sequence=NULL, componentTypeId=NULL, concentrationTypeId=NULL, externalId=NULL, density=NULL, abundance=NULL WHERE proteinId='${PROTEINID}';"
+mysql --defaults-file=${MYCNF} -s -D ${DB} -e "UPDATE Protein SET name='prot-${PROTEINID}', acronym='prot-${PROTEINID}', safetyLevel='GREEN', molecularMass=NULL, proteinType=NULL, sequence=NULL, componentTypeId=NULL, concentrationTypeId=NULL, externalId=NULL, density=NULL, abundance=NULL WHERE proteinId=${PROTEINID};"
