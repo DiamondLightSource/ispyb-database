6c8f64e829301870bc4547d825d5511328532483
Karl Levik <KarlLevik@users.noreply.github.com>
2020-05-20 16:30:21 +0100

    Script to remove a given PDB

---
+++ bin/forget_pdb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# This script removes information for a particular PDB.
+#
+# Usage:
+# ./forget_pdb.sh <.cnf file> <db_name> <proteinId>
+#
+# Usage example:
+# ./forget_pdb.sh ~/.my.cnf ispyb 21414
+#
+# Author: Karl Levik
+# Date: 2020-05-13
+
+HOST=localhost
+MYCNF=$1
+DB=$2
+PDBID=$3
+
+mysql --defaults-file=${MYCNF} -s -D ${DB} -e "UPDATE PDB SET name=pdbId, contents=NULL, code=NULL WHERE pdbId=${PDBID};"
