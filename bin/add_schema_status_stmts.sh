73beda12d868021a118d0627cb1478f9f5c74fc9
Karl Levik <KarlLevik@users.noreply.github.com>
2020-09-08 17:02:39 +0100

    Store only SQL file basename in tbl + preserve newlines

--- bin/add_schema_status_stmts.sh
+++ bin/add_schema_status_stmts.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+fname=$(basename ${1})
+
 sql=$(<${1})
-echo "INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('${1}', 'ONGOING');" > ${1}
+echo "INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('${fname}', 'ONGOING');" > ${1}
 echo >> ${1}
-echo ${sql} >> ${1}
+echo "${sql}" >> ${1}
 echo >> ${1}
-echo "UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '${1}';" >> ${1}
+echo "UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '${fname}';" >> ${1}
