270d9b5e6fa50482f4022328f1549a881e2e9212
Karl Levik <KarlLevik@users.noreply.github.com>
2020-10-02 17:27:55 +0100

    A little more info in column comments

--- schema/updates/2020_08_28_DiffractionPlan_new_cols.sql
+++ schema/updates/2020_08_28_DiffractionPlan_new_cols.sql
@@ -2,9 +2,9 @@ INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus)
   VALUES ('2020_08_28_DiffractionPlan_new_cols.sql', 'ONGOING');
 
 ALTER TABLE DiffractionPlan
-  ADD qMin float COMMENT 'minimum in qRange, unit: nm^-1',
-  ADD qMax float COMMENT 'maximum in qRange, unit: nm^-1',
-  ADD reductionParametersAveraging enum('All', 'Fastest Dimension', '1D') COMMENT '';
+  ADD qMin float COMMENT 'minimum in qRange, unit: nm^-1, needed for SAXS',
+  ADD qMax float COMMENT 'maximum in qRange, unit: nm^-1, needed for SAXS',
+  ADD reductionParametersAveraging enum('All', 'Fastest Dimension', '1D') COMMENT 'Post processing params for SAXS';
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' 
   WHERE scriptName = '2020_08_28_DiffractionPlan_new_cols.sql';
