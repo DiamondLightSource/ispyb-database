49e88b70bb63dce4dc92b3eb192d536d1a8b53dd
Stu Fisher <contact@roygbiv.org.uk>
2020-11-26 13:01:42 +0100

    rename

--- schema/updates/2020_11_22_blsample_staff_comments.sql
+++ schema/updates/2020_11_22_blsample_staff_comments.sql
@@ -1,6 +1,6 @@
-INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('20201122_blsample_staff_comments.sql', 'ONGOING');
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2020_11_22_blsample_staff_comments.sql', 'ONGOING');
 
 ALTER TABLE `BLSample`
 	ADD `staffComments` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Any staff comments on the sample';
 
-UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '20201122_blsample_staff_comments.sql';
+UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '2020_11_22_blsample_staff_comments.sql';
