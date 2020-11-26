e6803b394578fba75d5e7b4e37c5968e0ad96262
Stu Fisher <contact@roygbiv.org.uk>
2020-11-26 13:05:41 +0100

    rename

--- schema/updates/2020_07_31_extend_dcattachment_enum.sql
+++ schema/updates/2020_07_31_extend_dcattachment_enum.sql
@@ -1,6 +1,6 @@
-INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('20200731_extend_dcattachment_enum.sql', 'ONGOING');
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2020_07_31_extend_dcattachment_enum.sql', 'ONGOING');
 
 ALTER TABLE `DataCollectionFileAttachment`
     CHANGE `fileType` `fileType` ENUM('snapshot','log','xy','recip','pia','warning','params') DEFAULT NULL;
 
-UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '20200731_extend_dcattachment_enum.sql';
+UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '2020_07_31_extend_dcattachment_enum.sql';
