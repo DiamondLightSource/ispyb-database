e0f2f9218857eb818763285c689b51b77212c542
Stu Fisher <contact@roygbiv.org.uk>
2020-11-26 13:09:23 +0100

    rename and fix file reference

--- schema/updates/2020_07_31_extend_robotaction_enum.sql
+++ schema/updates/2020_07_31_extend_robotaction_enum.sql
@@ -1,6 +1,6 @@
-INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('20200731_extend_dcg_type_enum.sql', 'ONGOING');
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2020_07_31_extend_robotaction_enum.sql', 'ONGOING');
 
 ALTER TABLE `RobotAction`
 	CHANGE `actionType` `actionType` ENUM('LOAD','UNLOAD','DISPOSE','STORE','WASH','ANNEAL','MOSAIC') DEFAULT NULL;
 
-UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '20200731_extend_dcg_type_enum.sql';
+UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '2020_07_31_extend_robotaction_enum.sql';
