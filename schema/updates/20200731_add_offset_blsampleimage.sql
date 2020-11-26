ba710ee51ea38a4f46db940fb46bdba21c446be5
Stu Fisher <contact@roygbiv.org.uk>
2020-11-26 12:33:05 +0100

    camel, fix default

--- schema/updates/20200731_add_offset_blsampleimage.sql
+++ schema/updates/20200731_add_offset_blsampleimage.sql
@@ -1,7 +1,7 @@
 INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('20200731_add_offset_blsampleimage.sql', 'ONGOING');
 
 ALTER TABLE `BLSampleImage`
-	ADD `offsetx` INT NOT NULL DEFAULT '0' COMMENT 'The x offset of the image relative to the canvas',
-	ADD `offsety` INT NOT NULL DEFAULT '0' COMMENT 'The y offset of the image relative to the canvas';
+	ADD `offsetX` INT NOT NULL DEFAULT 0 COMMENT 'The x offset of the image relative to the canvas',
+	ADD `offsetY` INT NOT NULL DEFAULT 0 COMMENT 'The y offset of the image relative to the canvas';
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '20200731_add_offset_blsampleimage.sql';
