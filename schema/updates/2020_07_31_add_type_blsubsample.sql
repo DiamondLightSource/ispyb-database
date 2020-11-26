3b8cd7f779da7dfb3fb21a97115d8b35555ddf68
Stu Fisher <contact@roygbiv.org.uk>
2020-11-26 12:58:39 +0100

    rename

--- schema/updates/2020_07_31_add_type_blsubsample.sql
+++ schema/updates/2020_07_31_add_type_blsubsample.sql
@@ -1,6 +1,6 @@
-INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('20200731_add_type_blsample.sql', 'ONGOING');
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2020_07_31_add_type_blsubsample.sql', 'ONGOING');
 
 ALTER TABLE `BLSubSample`
 	ADD `type` VARCHAR(10) DEFAULT NULL COMMENT 'The type of subsample, i.e. roi (region), poi (point), loi (line)';
 
-UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '20200731_add_type_blsample.sql';
+UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '2020_07_31_add_type_blsubsample.sql';
