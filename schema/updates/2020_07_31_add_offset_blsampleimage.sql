3a7b583c3642658e2cd129dbbec9807a78d29fa5
Stu Fisher <contact@roygbiv.org.uk>
2020-11-26 13:03:57 +0100

    rename

--- schema/updates/2020_07_31_add_offset_blsampleimage.sql
+++ schema/updates/2020_07_31_add_offset_blsampleimage.sql
@@ -1,7 +1,7 @@
-INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('20200731_add_offset_blsampleimage.sql', 'ONGOING');
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2020_07_31_add_offset_blsampleimage.sql', 'ONGOING');
 
 ALTER TABLE `BLSampleImage`
 	ADD `offsetX` INT NOT NULL DEFAULT 0 COMMENT 'The x offset of the image relative to the canvas',
 	ADD `offsetY` INT NOT NULL DEFAULT 0 COMMENT 'The y offset of the image relative to the canvas';
 
-UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '20200731_add_offset_blsampleimage.sql';
+UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '2020_07_31_add_offset_blsampleimage.sql';
