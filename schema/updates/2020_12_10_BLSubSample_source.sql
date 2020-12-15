6d85e5f0f3216af7f7f6b16f4230bcde6f047ec2
Karl Levik <KarlLevik@users.noreply.github.com>
2020-12-15 12:46:46 +0000

    Use manual, auto as enum option to be consistent

--- schema/updates/2020_12_10_BLSubSample_source.sql
+++ schema/updates/2020_12_10_BLSubSample_source.sql
@@ -2,7 +2,7 @@ INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus)
   VALUES ('2020_12_10_BLSubSample_source.sql', 'ONGOING');
 
 ALTER TABLE BLSubSample
-  ADD source enum('user', 'software') DEFAULT 'user';
+  ADD source enum('manual', 'auto') DEFAULT 'manual';
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' 
   WHERE scriptName = '2020_12_10_BLSubSample_source.sql';
