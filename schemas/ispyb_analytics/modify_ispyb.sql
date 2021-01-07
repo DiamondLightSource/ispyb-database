dab56a29cab2e5fff4bfadeb2fb73447c5c55c77
Karl Levik <KarlLevik@users.noreply.github.com>
2021-01-07 17:06:50 +0000

    Create AutoProc* tables/views

--- schemas/ispyb_analytics/modify_ispyb.sql
+++ schemas/ispyb_analytics/modify_ispyb.sql
@@ -10,8 +10,10 @@ UPDATE BLSession bs
 ALTER TABLE BLSession
   ADD INDEX BLSession_instrumentId (instrumentId);
 
+
 INSERT INTO Proposal_has_Instrument (instrumentId, proposalId)
   SELECT DISTINCT instrumentId, proposalId FROM BLSession WHERE instrumentId IS NOT NULL;
+
 
 ALTER TABLE DataCollectionGroup
   ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
@@ -23,6 +25,7 @@ UPDATE DataCollectionGroup dcg
 ALTER TABLE DataCollectionGroup
   ADD INDEX DataCollectionGroup_instrumentId (instrumentId);
 
+
 ALTER TABLE DataCollection
   ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
 
@@ -32,6 +35,7 @@ UPDATE DataCollection dc
 
 ALTER TABLE DataCollection
   ADD INDEX DataCollection_instrumentId (instrumentId);
+
 
 ALTER TABLE EnergyScan
   ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
@@ -43,6 +47,7 @@ UPDATE EnergyScan es
 ALTER TABLE EnergyScan
   ADD INDEX EnergyScan_instrumentId (instrumentId);
 
+
 ALTER TABLE XFEFluorescenceSpectrum
   ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
 
@@ -52,3 +57,69 @@ UPDATE XFEFluorescenceSpectrum xs
 
 ALTER TABLE XFEFluorescenceSpectrum
   ADD INDEX XFEFluorescenceSpectrum_instrumentId (instrumentId);
+
+
+ALTER TABLE AutoProcIntegration
+  ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
+
+UPDATE AutoProcIntegration api
+  JOIN DataCollection dc ON dc.dataCollectionId = api.dataCollectionId
+  SET api.instrumentId = dc.instrumentId;
+
+ALTER TABLE AutoProcIntegration
+  ADD INDEX AutoProcIntegration_instrumentId (instrumentId);
+
+
+ALTER TABLE AutoProcProgram
+  ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
+
+UPDATE AutoProcProgram app
+  JOIN AutoProcIntegration api ON api.autoProcProgramId = app.autoProcProgramId
+  SET app.instrumentId = api.instrumentId;
+
+ALTER TABLE AutoProcProgram
+  ADD INDEX AutoProcProgram_instrumentId (instrumentId);
+
+
+ALTER TABLE AutoProcScaling_has_Int
+  ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
+
+UPDATE AutoProcScaling_has_Int apshi
+  JOIN AutoProcIntegration api ON api.autoProcIntegrationId = apshi.autoProcIntegrationId
+  SET apshi.instrumentId = api.instrumentId;
+
+ALTER TABLE AutoProcScaling_has_Int
+  ADD INDEX AutoProcScaling_has_Int_instrumentId (instrumentId);
+
+
+ALTER TABLE AutoProcScaling
+  ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
+
+UPDATE AutoProcScaling aps
+  JOIN AutoProcScaling_has_Int apshi ON apshi.autoProcScalingId = aps.autoProcScalingId
+  SET aps.instrumentId = apshi.instrumentId;
+
+ALTER TABLE AutoProcScaling
+  ADD INDEX AutoProcScaling_instrumentId (instrumentId);
+
+
+ALTER TABLE AutoProcScalingStatistics
+  ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
+
+UPDATE AutoProcScalingStatistics apss
+  JOIN AutoProcScaling aps ON aps.autoProcScalingId = apss.autoProcScalingId
+  SET apss.instrumentId = aps.instrumentId;
+
+ALTER TABLE AutoProcScalingStatistics
+  ADD INDEX AutoProcScaling_instrumentId (instrumentId);
+
+
+ALTER TABLE AutoProc
+  ADD instrumentId SMALLINT UNSIGNED INVISIBLE;
+
+UPDATE AutoProc ap
+  JOIN AutoProcScaling aps ON aps.autoProcId = ap.autoProcId
+  SET ap.instrumentId = aps.instrumentId;
+
+ALTER TABLE AutoProc
+  ADD INDEX AutoProc_instrumentId (instrumentId);
--- schemas/ispyb_analytics/views.sql
+++ schemas/ispyb_analytics/views.sql
@@ -6,52 +6,121 @@
 -- Recreate database
 
 CREATE OR REPLACE SQL SECURITY INVOKER VIEW BLSession AS
-  SELECT * 
-  FROM $ispyb.BLSession bs
+  SELECT *
+  FROM $ispyb.BLSession
   WHERE instrumentId IN (
-    SELECT instrumentId 
+    SELECT ihu.instrumentId
     FROM $ispyb.Instrument_has_Username ihu
     WHERE ihu.username = current_user()
   );
 
 CREATE OR REPLACE SQL SECURITY INVOKER VIEW DataCollectionGroup AS
-  SELECT * 
-  FROM $ispyb.DataCollectionGroup bs
+  SELECT *
+  FROM $ispyb.DataCollectionGroup
   WHERE instrumentId IN (
-    SELECT instrumentId 
+    SELECT ihu.instrumentId
     FROM $ispyb.Instrument_has_Username ihu
     WHERE ihu.username = current_user()
   );
 
 CREATE OR REPLACE SQL SECURITY INVOKER VIEW DataCollection AS
-  SELECT * 
-  FROM $ispyb.DataCollection bs
+  SELECT *
+  FROM $ispyb.DataCollection
   WHERE instrumentId IN (
-    SELECT instrumentId 
+    SELECT ihu.instrumentId
     FROM $ispyb.Instrument_has_Username ihu
     WHERE ihu.username = current_user()
   );
 
 CREATE OR REPLACE SQL SECURITY INVOKER VIEW EnergyScan AS
-  SELECT * 
-  FROM $ispyb.EnergyScan bs
+  SELECT *
+  FROM $ispyb.EnergyScan
   WHERE instrumentId IN (
-    SELECT instrumentId 
+    SELECT ihu.instrumentId
     FROM $ispyb.Instrument_has_Username ihu
     WHERE ihu.username = current_user()
   );
 
 CREATE OR REPLACE SQL SECURITY INVOKER VIEW XFEFluorescenceSpectrum AS
-  SELECT * 
-  FROM $ispyb.XFEFluorescenceSpectrum bs
+  SELECT *
+  FROM $ispyb.XFEFluorescenceSpectrum
   WHERE instrumentId IN (
-    SELECT instrumentId 
+    SELECT ihu.instrumentId
     FROM $ispyb.Instrument_has_Username ihu
     WHERE ihu.username = current_user()
   );
 
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW AutoProcIntegration AS
+  SELECT *
+  FROM $ispyb.AutoProcIntegration
+  WHERE instrumentId IN (
+    SELECT ihu.instrumentId
+    FROM $ispyb.Instrument_has_Username ihu
+    WHERE ihu.username = current_user()
+  );
 
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW AutoProcProgram AS
+  SELECT *
+  FROM $ispyb.AutoProcProgram
+  WHERE instrumentId IN (
+    SELECT ihu.instrumentId
+    FROM $ispyb.Instrument_has_Username ihu
+    WHERE ihu.username = current_user()
+  );
 
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW AutoProcProgramAttachment AS
+  SELECT appa.*
+  FROM $ispyb.AutoProcProgramAttachment appa
+    JOIN $ispyb.AutoProcProgram app ON app.autoProcProgramId = appa.autoProcProgramId
+  WHERE app.instrumentId IN (
+    SELECT ihu.instrumentId
+    FROM $ispyb.Instrument_has_Username ihu
+    WHERE ihu.username = current_user()
+  );
 
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW AutoProcProgramMessage AS
+  SELECT appm.*
+  FROM $ispyb.AutoProcProgramMessage appm
+    JOIN $ispyb.AutoProcProgram app ON app.autoProcProgramId = appm.autoProcProgramId
+  WHERE app.instrumentId IN (
+    SELECT ihu.instrumentId
+    FROM $ispyb.Instrument_has_Username ihu
+    WHERE ihu.username = current_user()
+  );
 
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW AutoProcScaling_has_Int AS
+  SELECT *
+  FROM $ispyb.AutoProcScaling_has_Int
+  WHERE instrumentId IN (
+    SELECT ihu.instrumentId
+    FROM $ispyb.Instrument_has_Username ihu
+    WHERE ihu.username = current_user()
+  );
+
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW AutoProcScaling AS
+  SELECT *
+  FROM $ispyb.AutoProcScaling
+  WHERE instrumentId IN (
+    SELECT ihu.instrumentId
+    FROM $ispyb.Instrument_has_Username ihu
+    WHERE ihu.username = current_user()
+  );
+
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW AutoProcScalingStatistics AS
+  SELECT *
+  FROM $ispyb.AutoProcScalingStatistics
+  WHERE instrumentId IN (
+    SELECT ihu.instrumentId
+    FROM $ispyb.Instrument_has_Username ihu
+    WHERE ihu.username = current_user()
+  );
+
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW AutoProc AS
+  SELECT *
+  FROM $ispyb.AutoProc
+  WHERE instrumentId IN (
+    SELECT ihu.instrumentId
+    FROM $ispyb.Instrument_has_Username ihu
+    WHERE ihu.username = current_user()
+  );
 
