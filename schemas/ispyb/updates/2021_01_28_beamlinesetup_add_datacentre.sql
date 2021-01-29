8e179d4da87015ce6f4bc12aa19ca4ffc215c97d
Stu Fisher <stu@q3f.org>
2021-01-29 12:36:21 +0100

    add preferredDataCentre to beamlinesetup

---
+++ schemas/ispyb/updates/2021_01_28_beamlinesetup_add_datacentre.sql
@@ -0,0 +1,6 @@
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2021_01_28_beamlinesetup_add_datacentre.sql', 'ONGOING');
+
+ALTER TABLE `BeamlineSetup`
+    ADD `preferredDataCentre` VARCHAR(30) NULL DEFAULT NULL COMMENT 'Relevant datacentre to use to process data from this beamline';
+
+UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '2021_01_28_beamlinesetup_add_datacentre.sql';
