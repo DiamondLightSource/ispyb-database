28dc0893f92433b040dddc5a56bf80ebf32fd1ee
Karl Levik <KarlLevik@users.noreply.github.com>
2021-03-05 17:11:50 +0000

    Update container type b21-8+1 again

--- schemas/ispyb/updates/2021_03_05_ContainerType_update.sql
+++ schemas/ispyb/updates/2021_03_05_ContainerType_update.sql
@@ -1,5 +1,7 @@
 INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2021_03_05_ContainerType_update.sql', 'ONGOING');
 
-UPDATE ContainerType SET dropPerWellX = 1, dropPerWellY = 1, dropHeight = 1, dropWidth = 1 WHERE containerTypeId = 1;
+UPDATE ContainerType 
+  SET capacity = 9, wellPerRow = 9, dropPerWellX = 1, dropPerWellY = 1, dropHeight = 1, dropWidth = 1, dropOffsetX = 0, dropOffsetY = 0, wellDrop = -1 
+  WHERE containerTypeId = 1;
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2021_03_05_ContainerType_update.sql';
