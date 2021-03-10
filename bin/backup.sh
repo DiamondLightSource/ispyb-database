7e19d3481bd00997fd5a8b5ac6361e1a112e5ea7
Karl Levik <KarlLevik@users.noreply.github.com>
2021-03-10 15:57:10 +0000

    Source new file with lookup tables, use that instead

--- bin/backup.sh
+++ bin/backup.sh
@@ -20,39 +20,7 @@ mysqldump --defaults-file=../.my.cnf ${OPTIONS} --skip-triggers --routines --no-
 
 mysqldump --defaults-file=../.my.cnf ${OPTIONS} --no-create-info --no-data "${DB}" | sed -e 's/DEFINER=[^*]*\*/\*/' >> ${OUT_DIR}/routines.sql
 
-LOOKUP_TABLES=(
-AdminVar
-SchemaStatus
-ComponentType
-ComponentSubType
-ConcentrationType
-InspectionType
-PlateType
-SessionType
-WorkflowType
-Schedule
-Imager
-Detector
-DewarLocationList
-BeamLineSetup
-BF_component
-BF_subcomponent
-BF_system
-Permission
-BLSampleImageAutoScoreSchema
-BLSampleImageAutoScoreClass
-SpaceGroup
-ProcessingPipeline
-ProcessingPipelineCategory
-BLSampleType
-PurificationColumn
-)
-
-LOOKUP_TABLES_STRING=''
-for TABLE in "${LOOKUP_TABLES[@]}"
-do :
-    LOOKUP_TABLES_STRING+=" ${TABLE}"
-done
+source lookup_tables.sh
 
 mysqldump --defaults-file=../.my.cnf ${OPTIONS} --skip-triggers --no-create-info --complete-insert ${DB} ${LOOKUP_TABLES_STRING} | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' > ${OUT_DIR}/lookups.sql
 
