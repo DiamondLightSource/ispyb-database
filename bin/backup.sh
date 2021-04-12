4b87d7a8bb29fbd9cd343a3e254c71048e7c7521
Karl Levik <KarlLevik@users.noreply.github.com>
2021-04-12 12:39:10 +0100

    Add --add-drop-trigger flag to drop trigger if exists before creation

--- bin/backup.sh
+++ bin/backup.sh
@@ -18,7 +18,7 @@ mysqldump --defaults-file=../.my.cnf ${OPTIONS} --skip-triggers --no-data "${DB}
 
 mysqldump --defaults-file=../.my.cnf ${OPTIONS} --skip-triggers --routines --no-create-info --no-data "${DB}" | sed -e 's/DEFINER=[^*]*\*/SQL SECURITY INVOKER \*/' | sed -e 's/CREATE .* PROCEDURE/CREATE PROCEDURE/' | sed -e 's/CREATE .* FUNCTION/CREATE FUNCTION/' > ${OUT_DIR}/routines.sql
 
-mysqldump --defaults-file=../.my.cnf ${OPTIONS} --no-create-info --no-data "${DB}" | sed -e 's/DEFINER=[^*]*\*/\*/' >> ${OUT_DIR}/routines.sql
+mysqldump --defaults-file=../.my.cnf ${OPTIONS} --no-create-info --no-data --add-drop-trigger "${DB}" | sed -e 's/DEFINER=[^*]*\*/\*/' >> ${OUT_DIR}/routines.sql
 
 source lookup_tables.sh
 
