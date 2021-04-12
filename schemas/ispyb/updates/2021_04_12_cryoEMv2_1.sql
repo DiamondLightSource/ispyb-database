5d1a0e6479df895cf1d1b3c6e5ef443d3375fb28
Karl Levik <KarlLevik@users.noreply.github.com>
2021-04-12 17:52:28 +0100

    Add intermediary table ParticleClassificationGroup, move columns to it

---
+++ schemas/ispyb/updates/2021_04_12_cryoEMv2_1.sql
@@ -0,0 +1,36 @@
+INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2021_04_12_cryoEMv2_1.sql', 'ONGOING');
+
+CREATE TABLE `ParticleClassificationGroup` (
+  particleClassificationGroupId int unsigned auto_increment PRIMARY KEY,
+  particlePickerId int unsigned,
+  programId int unsigned,
+  type enum('2D', '3D') COMMENT 'Indicates the type of particle classification',
+  batchNumber int unsigned COMMENT 'Corresponding to batch number',
+  numberOfParticlesPerBatch int unsigned COMMENT 'total number of particles per batch (a large integer)',
+  numberOfClassesPerBatch int unsigned,
+  symmetry varchar(20),
+  CONSTRAINT `ParticleClassificationGroup_fk_particlePickerId`
+    FOREIGN KEY (`particlePickerId`)
+      REFERENCES `ParticlePicker` (`particlePickerId`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ParticleClassificationGroup_fk_programId`
+    FOREIGN KEY (`programId`)
+      REFERENCES `AutoProcProgram` (`autoProcProgramId`)
+        ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
+ALTER TABLE `ParticlePicker`
+  DROP CONSTRAINT `ParticlePicker_fk_particleClassificationProgramId`,
+  DROP particleClassificationProgramId;
+
+ALTER TABLE `ParticleClassification`
+  ADD particleClassificationGroupId int unsigned,
+  DROP CONSTRAINT `ParticleClassification_fk_particlePickerId`,
+  DROP particlePickerId,
+  DROP `type`,
+  DROP batchNumber,
+  DROP numberOfParticlesPerBatch,
+  DROP numberOfClassesPerBatch,
+  DROP symmetry;
+
+UPDATE SchemaStatus SET schemaStatus = 'DONE' where scriptName = '2021_04_12_cryoEMv2_1.sql';
