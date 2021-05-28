d5ca8220b246346a372a213c89ab126197ebea6c
Angus Collett <angus.collett@diamond.ac.uk>
2021-05-28 12:53:54 +0100

    Refactor parameter names

--- schemas/ispyb/stored_programs/sf_retrieve_proposal_title_v2.sql
+++ schemas/ispyb/stored_programs/sf_retrieve_proposal_title_v2.sql
@@ -1,13 +1,13 @@
 DELIMITER ;;
-CREATE FUNCTION `retrieve_proposal_title_v2`(p_proposal_code varchar(5), p_proposal_number int) RETURNS varchar(255) CHARSET latin1
+CREATE FUNCTION `retrieve_proposal_title_v2`(p_proposalCode varchar(5), p_proposalNumber int) RETURNS varchar(255) CHARSET latin1
 READS SQL DATA
 COMMENT 'Retrieve the title for a given proposal code and number.'
 BEGIN
 	DECLARE ret_title varchar(255);
 	SELECT title INTO ret_title
 	FROM Proposal
-		WHERE proposalCode = p_proposal_code
-		AND proposalNumber = p_proposal_number
+		WHERE proposalCode = p_proposalCode
+		AND proposalNumber = p_proposalNumber
 	LIMIT 1;
 	RETURN ret_title;
 END ;;
