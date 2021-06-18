d95188977818537f2d19c0f11c0d0034b5688252
Karl Levik <KarlLevik@users.noreply.github.com>
2021-06-18 11:25:54 +0100

    capacity should be 192

--- schemas/ispyb/updates/2021_06_18_ContainerType_update.sql
+++ schemas/ispyb/updates/2021_06_18_ContainerType_update.sql
@@ -1,7 +1,7 @@
 INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2021_06_18_ContainerType_update.sql', 'ONGOING');
 
 UPDATE ContainerType 
-  SET capacity = 96, wellPerRow = 12, dropPerWellX = 2, dropPerWellY = 1, dropHeight = 0.5, dropWidth = 1, dropOffsetX = 0, dropOffsetY = 0, wellDrop = -1 
+  SET capacity = 192, wellPerRow = 12, dropPerWellX = 2, dropPerWellY = 1, dropHeight = 0.5, dropWidth = 1, dropOffsetX = 0, dropOffsetY = 0, wellDrop = -1 
   WHERE containerTypeId = 2;
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2021_06_18_ContainerType_update.sql';
