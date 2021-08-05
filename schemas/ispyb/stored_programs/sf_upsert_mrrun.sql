fa85d5c90c2ed354abd1332d7f60dff6d51b83c5
Richard Gildea <richard.gildea@diamond.ac.uk>
2021-08-05 15:35:11 +0100

    Update stored function/procedure

--- schemas/ispyb/stored_programs/sf_upsert_mrrun.sql
+++ schemas/ispyb/stored_programs/sf_upsert_mrrun.sql
@@ -1,14 +1,14 @@
-DELIMITER $$
-CREATE OR REPLACE FUNCTION `upsert_mrrun` (
+DELIMITER ;;
+CREATE FUNCTION `upsert_mrrun`(
      p_id integer,
      p_parentId integer,
      p_success boolean,
-     p_message varchar(255), 
+     p_message varchar(255),
      p_pipeline varchar(50),
-     p_inputCoordFile varchar(255), 
-     p_outputCoordFile varchar(255), 
-     p_inputMTZFile varchar(255), 
-     p_outputMTZFile varchar(255), 
+     p_inputCoordFile varchar(255),
+     p_outputCoordFile varchar(255),
+     p_inputMTZFile varchar(255),
+     p_outputMTZFile varchar(255),
      p_runDirectory varchar(255),
      p_logFile varchar(255),
      p_commandLine varchar(255),
@@ -18,56 +18,91 @@ CREATE OR REPLACE FUNCTION `upsert_mrrun` (
      p_rFreeValueEnd float ,
      p_starttime datetime,
      p_endtime datetime
-     )
-     RETURNs int
-  MODIFIES SQL DATA
-  BEGIN
+     ) RETURNS int(11)
+    MODIFIES SQL DATA
+BEGIN
 
-    --  Insert a new row, using all the parameters
-    INSERT INTO MXMRRun (mxMRRunId, autoProcScalingId, success, message, pipeline, inputCoordFile, outputCoordFile, inputMTZFile, outputMTZFile, 
-		runDirectory, logFile, commandLine, rValueStart, rValueEnd, rFreeValueStart, rFreeValueEnd, starttime, endtime) 
-      VALUES (
-        p_id, 
-        p_parentId, 
-        p_success, 
-        p_message, 
-        p_pipeline, 
-        p_inputCoordFile, 
-        p_outputCoordFile, 
-        p_inputMTZFile, 
-        p_outputMTZFile, 
-        p_runDirectory,
-        p_logFile,
-        p_commandLine,
-        p_rValueStart, 
-        p_rValueEnd, 
-        p_rFreeValueStart, 
-        p_rFreeValueEnd, 
-        p_starttime, 
-        p_endtime)
-		ON DUPLICATE KEY UPDATE
-			autoProcScalingId = IFNULL(p_parentId, autoProcScalingId), 
-            success = IFNULL(p_success, success), 
-            message = IFNULL(p_message, message), 
-            pipeline = IFNULL(p_pipeline, pipeline), 
-            inputCoordFile = IFNULL(p_inputCoordFile, inputCoordFile), 
-            outputCoordFile = IFNULL(p_outputCoordFile, outputCoordFile), 
-            inputMTZFile = IFNULL(p_inputMTZFile, inputMTZFile), 
-            outputMTZFile = IFNULL(p_outputMTZFile, outputMTZFile), 
-            runDirectory = IFNULL(p_runDirectory, runDirectory), 
-            logFile = IFNULL(p_logFile, logFile), 
-            commandLine = IFNULL(p_commandLine, commandLine), 
-            rValueStart = IFNULL(p_rValueStart, rValueStart), 
-            rValueEnd = IFNULL(p_rValueEnd, rValueEnd), 
-            rFreeValueStart = IFNULL(p_rFreeValueStart, rFreeValueStart), 
-            rFreeValueEnd = IFNULL(p_rFreeValueEnd, rFreeValueEnd), 
-            starttime = IFNULL(p_starttime, starttime), 
-            endtime = IFNULL(p_endtime, endtime);
- 
-	IF p_id IS NULL THEN 
-		RETURN LAST_INSERT_ID();
-    ELSE 
-		RETURN p_id;
-    END IF;
-  END$$
+  DECLARE app_id integer DEFAULT NULL;
+  DECLARE tmp_runDirectory varchar(255);
+
+  -- Insert/update the new MXMRRun rows
+  INSERT INTO MXMRRun (mxMRRunId, autoProcScalingId, rValueStart, rValueEnd, rFreeValueStart, rFreeValueEnd)
+    VALUES (
+      p_id,
+      p_parentId,
+      p_rValueStart,
+      p_rValueEnd,
+      p_rFreeValueStart,
+      p_rFreeValueEnd)
+  ON DUPLICATE KEY UPDATE
+    autoProcScalingId = IFNULL(p_parentId, autoProcScalingId),
+    rValueStart = IFNULL(p_rValueStart, rValueStart),
+    rValueEnd = IFNULL(p_rValueEnd, rValueEnd),
+    rFreeValueStart = IFNULL(p_rFreeValueStart, rFreeValueStart),
+    rFreeValueEnd = IFNULL(p_rFreeValueEnd, rFreeValueEnd)
+    ;
+
+	IF p_id IS NULL THEN
+		SET p_id = LAST_INSERT_ID();
+  END IF;
+
+  -- Get the autoProcProgramId for this row, if set (only applicable if updating)
+  SET app_id = (SELECT autoProcProgramId FROM MXMRRun WHERE mxMRRunId = p_id);
+
+  -- Insert/update the related AutoProcProgram entry
+  INSERT INTO AutoProcProgram (
+    autoProcProgramId, processingCommandLine, processingPrograms, processingStatus,
+    processingMessage, processingStartTime, processingEndTime, recordTimeStamp)
+    VALUES (app_id, p_commandLine, p_pipeline, p_success, p_message, p_starttime, p_endtime, p_endtime)
+  ON DUPLICATE KEY UPDATE
+    autoProcProgramId = autoProcProgramId,
+    processingCommandLine = IFNULL(p_commandLine, processingCommandLine),
+    processingPrograms = IFNULL(p_pipeline, processingPrograms),
+    processingStatus = IFNULL(p_success, processingStatus),
+    processingMessage = IFNULL(p_message, processingMessage),
+    processingStartTime = IFNULL(p_starttime, p_starttime),
+    processingEndTime = IFNULL(p_endtime, processingEndTime),
+    recordTimeStamp = IFNULL(p_endtime, recordTimeStamp)
+    ;
+
+  -- Set the autoProcProgramId in MXMRRun if not already set
+  IF app_id IS NULL THEN
+    SET app_id = LAST_INSERT_ID();
+    UPDATE MXMRRun SET autoProcProgramId = app_id WHERE mxMRRunId = p_id;
+  END IF;
+
+  -- mxmr_runDirectory with trailing space for use with REPLACE below
+  SET tmp_runDirectory = p_runDirectory;
+  IF tmp_runDirectory IS NOT NULL AND RIGHT(tmp_runDirectory, 1) != '/' THEN
+    SET tmp_runDirectory = CONCAT(tmp_runDirectory, '/') ;
+  END IF;
+
+  IF p_inputCoordFile IS NOT NULL THEN
+    INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp)
+      VALUES (app_id, "Input", p_runDirectory, REPLACE(p_inputCoordFile, tmp_runDirectory, ''), p_endtime);
+  END IF;
+
+  IF p_inputMTZFile IS NOT NULL THEN
+    INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp)
+      VALUES (app_id, "Input", p_runDirectory, REPLACE(p_inputMTZFile, tmp_runDirectory, ''), p_endtime);
+  END IF;
+
+  IF p_outputCoordFile IS NOT NULL THEN
+    INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp)
+      VALUES (app_id, "Result", p_runDirectory, REPLACE(p_outputCoordFile, tmp_runDirectory, ''), p_endtime);
+  END IF;
+
+  IF p_outputMTZFile IS NOT NULL THEN
+  INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp)
+    VALUES (app_id, "Result", p_runDirectory, REPLACE(p_outputMTZFile, tmp_runDirectory, ''), p_endtime);
+  END IF;
+
+  IF p_logFile IS NOT NULL THEN
+  INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp, importanceRank)
+    VALUES (app_id, "Log", p_runDirectory, REPLACE(p_logFile, tmp_runDirectory, ''), p_endtime, 0);
+  END IF;
+
+  RETURN p_id;
+
+  END ;;
 DELIMITER ;
--- schemas/ispyb/stored_programs/sp_upsert_mr_run.sql
+++ schemas/ispyb/stored_programs/sp_upsert_mr_run.sql
@@ -1,14 +1,14 @@
 DELIMITER ;;
-CREATE OR REPLACE DEFINER=`ispyb_root`@`%` PROCEDURE `upsert_mrrun`(
+CREATE PROCEDURE `upsert_mrrun`(
      INOUT p_id integer,
      p_parentId integer,
      p_success boolean,
-     p_message varchar(255), 
+     p_message varchar(255),
      p_pipeline varchar(50),
-     p_inputCoordFile varchar(255), 
-     p_outputCoordFile varchar(255), 
-     p_inputMTZFile varchar(255), 
-     p_outputMTZFile varchar(255), 
+     p_inputCoordFile varchar(255),
+     p_outputCoordFile varchar(255),
+     p_inputMTZFile varchar(255),
+     p_outputMTZFile varchar(255),
      p_runDirectory varchar(255),
      p_logFile varchar(255),
      p_commandLine varchar(255),
@@ -18,56 +18,103 @@ CREATE OR REPLACE DEFINER=`ispyb_root`@`%` PROCEDURE `upsert_mrrun`(
      p_rFreeValueEnd float ,
      p_starttime datetime,
      p_endtime datetime
-     ) 
+     )
     MODIFIES SQL DATA
-    COMMENT 'Update or insert new entry with info about a MX molecular replacements run, e.g. Dimple'    
+    COMMENT 'Update or insert new entry with info about a MX molecular replacements run, e.g. Dimple'
 BEGIN
+
+    DECLARE app_id integer DEFAULT NULL;
+    DECLARE tmp_runDirectory varchar(255);
+
     IF p_parentId IS NOT NULL THEN
-      INSERT INTO MXMRRun (mxMRRunId, autoProcScalingId, success, message, pipeline, inputCoordFile, outputCoordFile, inputMTZFile, outputMTZFile, 
-		runDirectory, logFile, commandLine, rValueStart, rValueEnd, rFreeValueStart, rFreeValueEnd, starttime, endtime) 
+
+      -- Insert/update the new MXMRRun rows
+      INSERT INTO MXMRRun (mxMRRunId, autoProcScalingId, rValueStart, rValueEnd, rFreeValueStart, rFreeValueEnd)
+        VALUES (
+          p_id,
+          p_parentId,
+          p_rValueStart,
+          p_rValueEnd,
+          p_rFreeValueStart,
+          p_rFreeValueEnd)
+      ON DUPLICATE KEY UPDATE
+        autoProcScalingId = IFNULL(p_parentId, autoProcScalingId),
+        rValueStart = IFNULL(p_rValueStart, rValueStart),
+        rValueEnd = IFNULL(p_rValueEnd, rValueEnd),
+        rFreeValueStart = IFNULL(p_rFreeValueStart, rFreeValueStart),
+        rFreeValueEnd = IFNULL(p_rFreeValueEnd, rFreeValueEnd)
+        ;
+
+      IF p_id IS NULL THEN
+        SET p_id = LAST_INSERT_ID();
+      END IF;
+
+      -- Get the autoProcProgramId for this row, if set (only applicable if updating)
+      SET app_id = (SELECT autoProcProgramId FROM MXMRRun WHERE mxMRRunId = p_id);
+
+      -- Insert/update the related AutoProcProgram entry
+      INSERT INTO AutoProcProgram (
+        autoProcProgramId, processingCommandLine, processingPrograms, processingStatus,
+        processingMessage, processingStartTime, processingEndTime, recordTimeStamp)
       VALUES (
-        p_id, 
-        p_parentId, 
-        p_success, 
-        substr(p_message, 1, 255),
-        substr(p_pipeline, 1, 50),
-        substr(p_inputCoordFile, 1, 255),
-        substr(p_outputCoordFile, 1, 255),
-        substr(p_inputMTZFile, 1, 255),
-        substr(p_outputMTZFile, 1, 255),
-        substr(p_runDirectory, 1, 255),
-        substr(p_logFile, 1, 255),
+        app_id,
         substr(p_commandLine, 1, 255),
-        p_rValueStart, 
-        p_rValueEnd, 
-        p_rFreeValueStart, 
-        p_rFreeValueEnd, 
-        IFNULL(p_starttime, NOW()), 
-        p_endtime)
-		ON DUPLICATE KEY UPDATE
-			autoProcScalingId = IFNULL(p_parentId, autoProcScalingId), 
-            success = IFNULL(p_success, success), 
-            message = IFNULL(substr(p_message, 1, 255), message), 
-            pipeline = IFNULL(substr(p_pipeline, 1, 50), pipeline), 
-            inputCoordFile = IFNULL(substr(p_inputCoordFile, 1, 255), inputCoordFile), 
-            outputCoordFile = IFNULL(substr(p_outputCoordFile, 1, 255), outputCoordFile), 
-            inputMTZFile = IFNULL(substr(p_inputMTZFile, 1, 255), inputMTZFile), 
-            outputMTZFile = IFNULL(substr(p_outputMTZFile, 1, 255), outputMTZFile), 
-            runDirectory = IFNULL(substr(p_runDirectory, 1, 255), runDirectory), 
-            logFile = IFNULL(substr(p_logFile, 1, 255), logFile), 
-            commandLine = IFNULL(substr(p_commandLine, 1, 255), commandLine), 
-            rValueStart = IFNULL(p_rValueStart, rValueStart), 
-            rValueEnd = IFNULL(p_rValueEnd, rValueEnd), 
-            rFreeValueStart = IFNULL(p_rFreeValueStart, rFreeValueStart), 
-            rFreeValueEnd = IFNULL(p_rFreeValueEnd, rFreeValueEnd), 
-            starttime = IFNULL(p_starttime, starttime), 
-            endtime = IFNULL(p_endtime, endtime);
- 
- 	  IF p_id IS NULL THEN 
-		SET p_id = LAST_INSERT_ID();
+        substr(p_pipeline, 1, 50),
+        p_success,
+        substr(p_message, 1, 255),
+        IFNULL(p_starttime, NOW()),
+        p_endtime,
+        IFNULL(p_endtime, NOW())
+        )
+      ON DUPLICATE KEY UPDATE
+        autoProcProgramId = autoProcProgramId,
+        processingCommandLine = IFNULL(substr(p_commandLine, 1, 255), processingCommandLine),
+        processingPrograms = IFNULL(substr(p_pipeline, 1, 50), processingPrograms),
+        processingStatus = IFNULL(p_success, processingStatus),
+        processingMessage = IFNULL(substr(p_message, 1, 255), processingMessage),
+        processingStartTime = IFNULL(p_starttime, processingStartTime),
+        processingEndTime = IFNULL(p_endtime, processingEndTime),
+        recordTimeStamp = IFNULL(p_endtime, recordTimeStamp)
+        ;
+
+      -- Set the autoProcProgramId in MXMRRun if not already set
+      IF app_id IS NULL THEN
+        SET app_id = LAST_INSERT_ID();
+        UPDATE MXMRRun SET autoProcProgramId = app_id WHERE mxMRRunId = p_id;
+      END IF;
+
+      -- mxmr_runDirectory with trailing space for use with REPLACE below
+      SET tmp_runDirectory = p_runDirectory;
+      IF tmp_runDirectory IS NOT NULL AND RIGHT(tmp_runDirectory, 1) != '/' THEN
+        SET tmp_runDirectory = CONCAT(tmp_runDirectory, '/') ;
+      END IF;
+
+      IF p_inputCoordFile IS NOT NULL THEN
+        INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp)
+          VALUES (app_id, "Input", p_runDirectory, REPLACE(p_inputCoordFile, tmp_runDirectory, ''), p_endtime);
+      END IF;
+
+      IF p_inputMTZFile IS NOT NULL THEN
+        INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp)
+          VALUES (app_id, "Input", p_runDirectory, REPLACE(p_inputMTZFile, tmp_runDirectory, ''), p_endtime);
+      END IF;
+
+      IF p_outputCoordFile IS NOT NULL THEN
+        INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp)
+          VALUES (app_id, "Result", p_runDirectory, REPLACE(p_outputCoordFile, tmp_runDirectory, ''), p_endtime);
+      END IF;
+
+      IF p_outputMTZFile IS NOT NULL THEN
+      INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp)
+        VALUES (app_id, "Result", p_runDirectory, REPLACE(p_outputMTZFile, tmp_runDirectory, ''), p_endtime);
+      END IF;
+
+      IF p_logFile IS NOT NULL THEN
+      INSERT INTO AutoProcProgramAttachment (autoProcProgramId, fileType, filePath, fileName, recordTimeStamp, importanceRank)
+        VALUES (app_id, "Log", p_runDirectory, REPLACE(p_logFile, tmp_runDirectory, ''), p_endtime, 0);
       END IF;
 	ELSE
 	  SIGNAL SQLSTATE '45000' SET MYSQL_ERRNO=1644, MESSAGE_TEXT='Mandatory argument p_parentId can not be NULL';
 	END IF;
-  END;;
+  END ;;
 DELIMITER ;
