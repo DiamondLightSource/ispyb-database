a3e46abfdb7508ab0cf4cfe4e0e96f23f1e2653f
Stuart Fisher <stuart.fisher@esrf.fr>
2022-06-28 16:07:58 +0200

    camelCase

--- schemas/ispyb/updates/2022_06_28_gridinfo_patches.sql
+++ schemas/ispyb/updates/2022_06_28_gridinfo_patches.sql
@@ -1,7 +1,7 @@
 INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2022_06_28_gridinfo_patches.sql', 'ONGOING');
 
 ALTER TABLE GridInfo
-    ADD patches_x int(10) DEFAULT 1 COMMENT 'Number of patches the grid is made up of in the X direction',
-    ADD patches_y int(10) DEFAULT 1 COMMENT 'Number of patches the grid is made up of in the Y direction';
+    ADD patchesX int(10) DEFAULT 1 COMMENT 'Number of patches the grid is made up of in the X direction',
+    ADD patchesY int(10) DEFAULT 1 COMMENT 'Number of patches the grid is made up of in the Y direction';
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2022_06_28_gridinfo_patches.sql';
