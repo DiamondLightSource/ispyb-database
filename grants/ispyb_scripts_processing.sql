a00f6980d09a4222dc5760e271b18a75a0ef86c5
KarlLevik <KarlLevik@users.noreply.github.com>
2022-07-21 10:42:21 +0100

    Add role for processing scripts

---
+++ grants/ispyb_scripts_processing.sql
@@ -0,0 +1,20 @@
+-- Create the processing scripts role
+CREATE ROLE IF NOT EXISTS ispyb_scripts_processing;
+
+-- You must also create a database user and grant this role to them, e.g.
+-- CREATE USER ispyb_mx_scripts@'%' IDENTIFIED BY 'the_password';
+-- GRANT ispyb_scripts_processing to ispyb_mx_scripts@'%';
+-- SET DEFAULT ROLE ispyb_scripts_processing FOR ispyb_mx_scripts@'%';
+
+GRANT SELECT ON AutoProc TO ispyb_scripts_processing; 
+GRANT SELECT ON AutoProcIntegration TO ispyb_scripts_processing;
+GRANT SELECT ON AutoProcProgram TO ispyb_scripts_processing;
+GRANT SELECT ON AutoProcProgramAttachment TO ispyb_scripts_processing;
+GRANT SELECT ON AutoProcProgramMessage TO ispyb_scripts_processing;
+GRANT SELECT ON AutoProcScaling TO ispyb_scripts_processing;
+GRANT SELECT ON AutoProcScaling_has_Int TO ispyb_scripts_processing;
+GRANT SELECT ON AutoProc TO ispyb_scripts_processing;
+GRANT SELECT ON AutoProcScalingStatistics TO ispyb_scripts_processing;
+GRANT SELECT ON BLSession TO ispyb_scripts_processing;
+GRANT SELECT ON DataCollection TO ispyb_scripts_processing;
+GRANT SELECT ON Proposal TO ispyb_scripts_processing;
