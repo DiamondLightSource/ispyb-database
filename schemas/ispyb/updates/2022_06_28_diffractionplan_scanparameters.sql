772aaf4e7af71746930b0492910dbfdb60c389f4
Stuart Fisher <stuart.fisher@esrf.fr>
2022-08-16 12:10:20 +0200

    typo

--- schemas/ispyb/updates/2022_06_28_diffractionplan_scanparameters.sql
+++ schemas/ispyb/updates/2022_06_28_diffractionplan_scanparameters.sql
@@ -1,6 +1,6 @@
 INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2022_06_28_diffractionplan_scanparameters.sql', 'ONGOING');
 
 ALTER TABLE DiffractionPlan
-    ADD `scanParameters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`scanParameters`)) COMMENT 'JSON seralised scan parameters, useful for parameters without designated columns';
+    ADD `scanParameters` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`scanParameters`)) COMMENT 'JSON serialised scan parameters, useful for parameters without designated columns';
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2022_06_28_diffractionplan_scanparameters.sql';
