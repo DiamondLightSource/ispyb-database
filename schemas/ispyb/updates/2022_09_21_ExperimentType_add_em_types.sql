fabf4f0c1245c9f129dc490739f459fc29e56a1a
KarlLevik <KarlLevik@users.noreply.github.com>
2022-09-21 08:08:50 +0100

    Add experiment types for EM

--- schemas/ispyb/updates/2022_09_21_ExperimentType_add_em_types.sql
+++ schemas/ispyb/updates/2022_09_21_ExperimentType_add_em_types.sql
@@ -6,7 +6,7 @@ INSERT INTO ExperimentType (experimentTypeId, name, proposalType, active)
 ALTER TABLE DataCollectionGroup
   MODIFY experimentType enum('SAD','SAD - Inverse Beam','OSC','Collect - Multiwedge','MAD','Helical','Multi-positional','Mesh','Burn','MAD - Inverse Beam',
     'Characterization','Dehydration','tomo','experiment','EM','PDF','PDF+Bragg','Bragg','single particle','Serial Fixed','Serial Jet','Standard',
-    'Time Resolved','Diamond Anvil High Pressure','Custom','XRF map','Energy scan','XRF spectrum','XRF map xas','Mesh3D','Screening') 
+    'Time Resolved','Diamond Anvil High Pressure','Custom','XRF map','Energy scan','XRF spectrum','XRF map xas','Mesh3D','Screening','Tomography', 'Single Particle') 
     DEFAULT NULL
     COMMENT 'Standard: Routine structure determination experiment. Time Resolved: Investigate the change of a system over time. Custom: Special or non-standard data collection.';
 
