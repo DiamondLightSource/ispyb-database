ae64bcc691ea6d9b49c863970092dea96d2b4d7d
KarlLevik <KarlLevik@users.noreply.github.com>
2023-05-05 15:23:28 +0100

    Add default value

--- schemas/ispyb/updates/2023_05_05_ParticleClassificationGroup_selected.sql
+++ schemas/ispyb/updates/2023_05_05_ParticleClassificationGroup_selected.sql
@@ -1,7 +1,7 @@
 INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2023_05_05_ParticleClassificationGroup_selected.sql', 'ONGOING');
 
 ALTER TABLE ParticleClassificationGroup
-  ADD selected boolean COMMENT 'Indicates whether the group is selected for processing or not.';
+  ADD selected boolean DEFAULT False COMMENT 'Indicates whether the group is selected for processing or not.';
 
 -- Undo:
 -- ALTER TABLE ParticleClassificationGroup
