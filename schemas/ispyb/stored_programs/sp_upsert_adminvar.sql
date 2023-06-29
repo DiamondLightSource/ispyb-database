d568d44c55c4e2e204a5701f4728c2d5d6dd40e3
KarlLevik <KarlLevik@users.noreply.github.com>
2023-06-29 16:39:29 +0100

    New version of proc which is using `name` as they key

--- schemas/ispyb/stored_programs/sp_upsert_adminvar.sql
+++ schemas/ispyb/stored_programs/sp_upsert_adminvar.sql
@@ -4,19 +4,26 @@
 
 DELIMITER ;;
 CREATE OR REPLACE DEFINER=`ispyb_root`@`%` PROCEDURE `upsert_adminvar`(
-  INOUT p_id int(11),
   p_name varchar(32),
   p_value varchar(1024)
 )
     MODIFIES SQL DATA
 BEGIN
-    INSERT INTO AdminVar (varId, `name`, `value`)
-      VALUES (p_id, p_name, p_value)
-      ON DUPLICATE KEY UPDATE
-	  `name` = IFNULL(p_name, `name`),
-    `value` = IFNULL(p_value, `value`);
-  IF LAST_INSERT_ID() <> 0 THEN
-	  SET p_id = LAST_INSERT_ID();
+    DECLARE row_varId int(11) DEFAULT NULL;
+    DECLARE row_value varchar(1024) DEFAULT NULL;
+
+    START TRANSACTION;
+    SELECT varId, `value`
+      INTO row_varId, row_value
+      FROM AdminVar
+      WHERE `name` = p_name;
+
+    IF row_varId IS NULL THEN
+      INSERT INTO AdminVar (`name`, `value`)
+        VALUES (p_name, p_value);
+    ELSE
+      UPDATE AdminVar SET `value` = p_value WHERE `name` = p_name;
     END IF;
+    COMMIT;
 END ;;
 DELIMITER ;
