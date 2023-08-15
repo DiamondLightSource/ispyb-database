a608715ceaea4955e47a7c681c65fe6658510fc8
KarlLevik <KarlLevik@users.noreply.github.com>
2023-08-15 14:56:09 +0100

    New maintenance script

---
+++ schemas/ispyb/scripts/MotionCorrectionDrift_delete.sql
@@ -0,0 +1,24 @@
+-- Delete rows from table in batches
+
+DELIMITER ;;
+
+BEGIN NOT ATOMIC
+
+  SET @step := 1000000;
+  SET @id := @step;
+  SET @max := 1200000000;
+
+  REPEAT
+    SELECT @id AS '@id';
+    DELETE FROM MotionCorrectionDrift WHERE motionCorrectionDriftId < @id;
+    SET @id := @id + @step;
+    UNTIL @id > @max
+  END REPEAT;
+
+END;;
+
+DELIMITER ;
+
+-- Rebuild table to reclaim space - assumes InnoDB and requires innodb_file_per_table=ON:
+-- Can take a long time for large tables, should probably run in RSU (or NBO) mode on Galera
+ALTER TABLE MotionCorrectionDrift FORCE;
