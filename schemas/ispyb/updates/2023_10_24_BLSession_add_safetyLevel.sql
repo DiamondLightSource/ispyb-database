d9d98d29725b4edc6e4824ba76bfd11f1fee5382
KarlLevik <KarlLevik@users.noreply.github.com>
2023-10-24 15:23:01 +0100

    Rm old SQL file

--- schemas/ispyb/updates/2023_10_24_BLSession_add_safetyLevel.sql
+++
@@ -1,6 +0,0 @@
-INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2023_10_24_BLSession_add_riskRating.sql', 'ONGOING');
-
-ALTER TABLE BLSession
-    ADD riskRating enum('Low','Medium','High', 'Not Permitted') COMMENT 'ERA in user admin system';
-
-UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2023_10_24_BLSession_add_riskRating.sql';
