c78fa6e8974800f9dea3a3538075c8c457718383
KarlLevik <KarlLevik@users.noreply.github.com>
2024-01-08 17:03:18 +0000

    Bump version and update lookup schema file

--- schemas/ispyb/lookups.sql
+++ schemas/ispyb/lookups.sql
@@ -10,7 +10,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 /*!40000 ALTER TABLE `AdminVar` DISABLE KEYS */;
-INSERT INTO `AdminVar` (`varId`, `name`, `value`) VALUES (4,'schemaVersion','3.0.0');
+INSERT INTO `AdminVar` (`varId`, `name`, `value`) VALUES (4,'schemaVersion','4.0.0');
 /*!40000 ALTER TABLE `AdminVar` ENABLE KEYS */;
 
 /*!40000 ALTER TABLE `SchemaStatus` DISABLE KEYS */;
@@ -243,7 +243,8 @@ INSERT INTO `SchemaStatus` (`schemaStatusId`, `scriptName`, `schemaStatus`, `rec
 (269,'2024_01_02_drop_bioSAXS.sql','DONE','2024-01-08 15:41:22'),
 (270,'2024_01_02_drop_views.sql','DONE','2024-01-08 15:41:22'),
 (271,'2024_01_04_BLSession_drop_cols.sql','DONE','2024-01-08 15:41:22'),
-(272,'2024_01_04_drop_unused_tables.sql','DONE','2024-01-08 15:41:22');
+(272,'2024_01_04_drop_unused_tables.sql','DONE','2024-01-08 15:41:22'),
+(274,'2024_01_08_AdminVar_bump_version.sql','DONE','2024-01-08 17:01:43');
 /*!40000 ALTER TABLE `SchemaStatus` ENABLE KEYS */;
 
 /*!40000 ALTER TABLE `ComponentType` DISABLE KEYS */;
