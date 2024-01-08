0bf3a622d88488d5fb8c7cc369c847143b14f267
KarlLevik <KarlLevik@users.noreply.github.com>
2024-01-08 15:05:59 +0000

    Fix ERROR 1067 (42000): Invalid default value for 'lastUpdate'

--- schemas/ispyb/updates/2024_01_04_BLSession_drop_cols.sql
+++ schemas/ispyb/updates/2024_01_04_BLSession_drop_cols.sql
@@ -1,6 +1,7 @@
 INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2024_01_04_BLSession_drop_cols.sql', 'ONGOING');
 
 ALTER TABLE BLSession
+  MODIFY `lastUpdate` timestamp NOT NULL DEFAULT current_timestamp COMMENT 'last update timestamp: by default the end of the session, the last collect...',
   DROP COLUMN IF EXISTS projectCode,
   DROP COLUMN IF EXISTS sessionTitle,
   DROP COLUMN IF EXISTS structureDeterminations,
