d41bfcaf44751a188f315dd151735da03a0159ff
Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>
2024-09-04 16:32:14 +0100

    Update ProcessedTomogram comment (#241)

---
+++ schemas/ispyb/updates/2024_09_04_ProcessedTomogram_comment.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2024_09_04_ProcessedTomogram_comment', 'ONGOING');
+
+ALTER TABLE ProcessedTomogram COMMENT = 'Indicates the sample''s location on a multi-sample pin, where 1 is closest to the pin base or a sample''s position in a cryo-EM cassette';
+
+UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2024_09_04_ProcessedTomogram_comment.sql';
