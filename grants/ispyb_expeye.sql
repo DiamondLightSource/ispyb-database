86824ccc6f80a6f86e28bc78548ec3bd247e3bef
KarlLevik <KarlLevik@users.noreply.github.com>
2025-02-14 15:03:11 +0000

    Add more processing table grants for Expeye

--- grants/ispyb_expeye.sql
+++ grants/ispyb_expeye.sql
@@ -63,3 +63,7 @@ GRANT SELECT, INSERT, UPDATE ON RobotAction TO ispyb_expeye_acquisition_role;
 GRANT SELECT, INSERT, UPDATE ON GridInfo TO ispyb_expeye_acquisition_role;
 
 GRANT ispyb_expeye_acquisition_role TO ispyb_expeye_core_role;
+
+GRANT SELECT ON AutoProcProgram TO ispyb_expeye_core_role;
+GRANT SELECT, INSERT, UPDATE ON AutoProcProgramAttachment TO ispyb_expeye_core_role;
+GRANT SELECT ON ProcessingJob TO ispyb_expeye_core_role;
