fee2848e31a92f63887691a98170466f60acf228
KarlLevik <KarlLevik@users.noreply.github.com>
2025-06-27 17:16:14 +0100

    Refresh schema files

--- schemas/ispyb/routines.sql
+++ schemas/ispyb/routines.sql
@@ -4190,25 +4190,27 @@ BEGIN
             SELECT
                 d.dewarId "id", d.shippingId "shippingId", d.code "name", d.comments "comments", d.storageLocation "storageLocation",
                 d.dewarStatus "status", d.isStorageDewar "isStorageDewar", d.barCode "barCode", d.firstExperimentId "firstSessionId",
-				d.type "type", d.facilityCode "facilityCode", d.weight "weight", d.deliveryAgent_barcode "deliveryAgentBarcode"
+				        dr.type "type", d.facilityCode "facilityCode", d.weight "weight", d.deliveryAgent_barcode "deliveryAgentBarcode"
             FROM Dewar d
+                LEFT JOIN DewarRegistry dr ON dr.facilityCode = d.facilityCode
                 INNER JOIN Shipping s ON s.shippingId = d.shippingId
-		        INNER JOIN Proposal p ON p.proposalId = s.proposalId
+		            INNER JOIN Proposal p ON p.proposalId = s.proposalId
                 INNER JOIN BLSession bs ON bs.proposalId = s.proposalId
                 INNER JOIN Session_has_Person shp ON bs.sessionId = shp.sessionId
                 INNER JOIN Person per ON per.personId = shp.personId
             WHERE per.login = p_authLogin AND p.proposalCode = p_proposalCode AND p.proposalNumber = p_proposalNumber
             GROUP BY d.dewarId, d.shippingId, d.code, d.comments, d.storageLocation,
                 d.dewarStatus, d.isStorageDewar, d.barCode, d.firstExperimentId,
-				d.type, d.facilityCode, d.weight, d.deliveryAgent_barcode;
+				        dr.type, d.facilityCode, d.weight, d.deliveryAgent_barcode;
         ELSE
             SELECT
                 d.dewarId "id", d.shippingId "shippingId", d.code "name", d.comments "comments", d.storageLocation "storageLocation",
                 d.dewarStatus "status", d.isStorageDewar "isStorageDewar", d.barCode "barCode", d.firstExperimentId "firstSessionId",
-				d.type "type", d.facilityCode "facilityCode", d.weight "weight", d.deliveryAgent_barcode "deliveryAgentBarcode"
+				        dr.type "type", d.facilityCode "facilityCode", d.weight "weight", d.deliveryAgent_barcode "deliveryAgentBarcode"
             FROM Dewar d
+                LEFT JOIN DewarRegistry dr ON dr.facilityCode = d.facilityCode
                 INNER JOIN Shipping s ON s.shippingId = d.shippingId
-		        INNER JOIN Proposal p ON p.proposalId = s.proposalId
+		            INNER JOIN Proposal p ON p.proposalId = s.proposalId
             WHERE p.proposalCode = p_proposalCode AND p.proposalNumber = p_proposalNumber;
         END IF;
 
@@ -9984,6 +9986,112 @@ BEGIN
 
 		IF row_storageLocation <> p_storageLocation THEN
 
+      UPDATE Container
+			SET sampleChangerLocation = '', containerStatus = 'at facility'
+			WHERE dewarId = p_id;
+		END IF;
+	END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `upsert_dewar_v3` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb3 */ ;
+/*!50003 SET character_set_results = utf8mb3 */ ;
+/*!50003 SET collation_connection  = utf8mb3_general_ci */ ;
+DELIMITER ;;
+CREATE PROCEDURE `upsert_dewar_v3`(
+
+	 INOUT p_id int(10) unsigned,
+	 p_authLogin varchar(45),
+	 p_shippingId int(10) unsigned,
+	 p_name varchar(45),
+	 p_comments tinytext,
+	 p_storageLocation varchar(45),
+	 p_status varchar(45),
+	 p_isStorageDewar tinyint(1),
+	 p_barcode varchar(45),
+	 p_firstSessionId int(10) unsigned,
+	 p_customsValue int(11) unsigned,
+	 p_transportValue int(11) unsigned,
+	 p_trackingNumberToSynchrotron varchar(30),
+	 p_trackingNumberFromSynchrotron varchar(30),
+	 p_facilityCode varchar(20),
+	 p_weight float,
+	 p_deliveryAgentBarcode varchar(30)
+ )
+    MODIFIES SQL DATA
+    COMMENT 'Inserts or updates info about a dewar/parcel (p_id).\nMandatory columns:\nFor insert: none\nFor update: p_id \nReturns: Record ID in p_id.'
+BEGIN
+	DECLARE row_storageLocation varchar(45) DEFAULT NULL;
+	DECLARE row_dewarStatus varchar(45) DEFAULT NULL;
+  DECLARE row_count int unsigned DEFAULT 0;
+
+	IF p_storageLocation IS NULL THEN
+		SIGNAL SQLSTATE '45000' SET MYSQL_ERRNO=1644, MESSAGE_TEXT='Mandatory argument p_storageLocation is NULL';
+	END IF;
+
+  IF p_authLogin IS NOT NULL AND p_shippingId IS NOT NULL THEN
+
+    
+    
+    SELECT count(*) INTO row_count
+    FROM Shipping s
+      INNER JOIN BLSession bs ON bs.proposalId = s.proposalId
+      INNER JOIN Session_has_Person shp ON bs.sessionId = shp.sessionId
+      INNER JOIN Person p ON p.personId = shp.personId
+    WHERE p.login = p_authLogin AND s.shippingId = p_shippingId;
+
+    IF row_count = 0 THEN
+        SIGNAL SQLSTATE '02000'
+            SET MYSQL_ERRNO=1643, MESSAGE_TEXT = 'Dewar not in a shipping belonging to one of the p_authLogin Person sessions';
+    END IF;
+  END IF;
+
+  IF p_id IS NULL THEN
+    INSERT INTO Dewar(dewarId,shippingId,code,comments,storageLocation,dewarStatus,isStorageDewar,barCode,firstExperimentId,customsValue,transportValue,
+	    trackingNumberToSynchrotron,trackingNumberFromSynchrotron,FACILITYCODE,weight,deliveryAgent_barcode)
+	    VALUES (p_id, p_shippingId, p_name, p_comments, p_storageLocation, p_status, p_isStorageDewar, p_barcode, p_firstSessionId, p_customsValue, p_transportValue,
+		    p_trackingNumberToSynchrotron, p_trackingNumberFromSynchrotron, p_facilityCode, p_weight, p_deliveryAgentBarcode);
+	  SET p_id = LAST_INSERT_ID();
+	ELSE
+
+    SELECT storageLocation, dewarStatus INTO row_storageLocation, row_dewarStatus FROM Dewar WHERE dewarId = p_id;
+
+		UPDATE Dewar
+			SET shippingId = IFNULL(p_shippingId, shippingId),
+			code = IFNULL(p_name, code),
+			comments = IFNULL(p_comments, comments),
+			storageLocation = IFNULL(p_storageLocation, storageLocation),
+			dewarStatus = IFNULL(p_status, dewarStatus),
+			isStorageDewar = IFNULL(p_isStorageDewar, isStorageDewar),
+			barCode = IFNULL(p_barcode, barCode),
+			firstExperimentId = IFNULL(p_firstSessionId, firstExperimentId),
+			customsValue = IFNULL(p_customsValue, customsValue),
+			transportValue = IFNULL(p_transportValue, transportValue),
+			trackingNumberToSynchrotron = IFNULL(p_trackingNumberToSynchrotron, trackingNumberToSynchrotron),
+			trackingNumberFromSynchrotron = IFNULL(p_trackingNumberFromSynchrotron, trackingNumberFromSynchrotron),
+			FACILITYCODE = IFNULL(p_facilityCode, FACILITYCODE),
+			weight = IFNULL(p_weight, weight),
+			deliveryAgent_barcode = IFNULL(p_deliveryAgentBarcode, deliveryAgent_barcode)
+		WHERE dewarId = p_id;
+
+		
+    IF row_storageLocation <> p_storageLocation OR row_dewarStatus <> p_status THEN
+      INSERT INTO DewarTransportHistory (dewarId, dewarStatus, storageLocation, arrivalDate)
+				VALUES (p_id, p_status, p_storageLocation, NOW());
+    END IF;
+
+		
+		IF row_storageLocation <> p_storageLocation THEN
+			
       UPDATE Container
 			SET sampleChangerLocation = '', containerStatus = 'at facility'
 			WHERE dewarId = p_id;
