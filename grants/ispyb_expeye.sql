ca39aa949b1fb7c3a69dcf13962be3b8b531e0e5
Karl Levik <KarlLevik@users.noreply.github.com>
2025-07-24 17:36:09 +0100

    Add grants on Protein_has_PDB for expeye role (#295)

--- grants/ispyb_expeye.sql
+++ grants/ispyb_expeye.sql
@@ -75,3 +75,4 @@ GRANT SELECT ON ProcessingJob TO ispyb_expeye_core_role;
 GRANT SELECT, INSERT, UPDATE ON Ligand TO ispyb_expeye_core_role;
 GRANT SELECT, INSERT, UPDATE, DELETE ON Ligand_has_PDB TO ispyb_expeye_core_role;
 GRANT SELECT, INSERT, UPDATE, DELETE ON BLSample_has_Ligand TO ispyb_expeye_core_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Protein_has_PDB TO ispyb_expeye_core_role;
