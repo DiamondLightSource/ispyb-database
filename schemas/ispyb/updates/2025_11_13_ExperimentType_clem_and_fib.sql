edfd09140f9197dd818548a5f7553b2231ee31af
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2025-11-13 15:29:14 +0000

    Fix syntax

--- schemas/ispyb/updates/2025_11_13_ExperimentType_clem_and_fib.sql
+++ schemas/ispyb/updates/2025_11_13_ExperimentType_clem_and_fib.sql
@@ -2,9 +2,6 @@ INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2025_11_13_E
 
 INSERT INTO ExperimentType (
   experimentTypeId, name, proposalType, active
-) VALUES (
-  45, 'CLEM', 'em', 1,
-  46, 'FIB', 'em', 1
-);
+) VALUES (45, 'CLEM', 'em', 1), (46, 'FIB', 'em', 1);
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2025_11_13_ExperimentType_clem_and_fib.sql';
