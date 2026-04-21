15eadcf9af6e555f35565da31e0b740e9f05183d
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2026-04-21 15:02:12 +0100

    Update file date

--- schemas/ispyb/updates/2026_04_22_GridInfo_pixelsPerMicron.sql
+++ schemas/ispyb/updates/2026_04_22_GridInfo_pixelsPerMicron.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2025_03_21_GridInfo_pixelsPerMicron.sql', 'ONGOING');
+INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2026_04_22_GridInfo_pixelsPerMicron.sql', 'ONGOING');
 
 UPDATE GridInfo SET micronsPerPixelX = pixelsPerMicronX WHERE micronsPerPixelX IS NULL;
 UPDATE GridInfo SET micronsPerPixelY = pixelsPerMicronY WHERE micronsPerPixelY IS NULL;
@@ -7,4 +7,4 @@ ALTER TABLE GridInfo
 	DROP COLUMN pixelsPerMicronX,
 	DROP COLUMN pixelsPerMicronY;
 
-UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2025_03_21_GridInfo_pixelsPerMicron.sql';
+UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2026_04_22_GridInfo_pixelsPerMicron.sql';
