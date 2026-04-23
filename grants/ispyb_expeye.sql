8481f3c240f1413f0fc6a8c67999ca2d17b22213
Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>
2026-04-23 16:19:50 +0100

    Update Expeye grants

--- grants/ispyb_expeye.sql
+++ grants/ispyb_expeye.sql
@@ -74,6 +74,7 @@ GRANT ispyb_expeye_acquisition_role TO ispyb_expeye_core_role;
 GRANT SELECT, INSERT, UPDATE ON AutoProcProgram TO ispyb_expeye_core_role;
 GRANT SELECT, INSERT, UPDATE ON AutoProcProgramAttachment TO ispyb_expeye_core_role;
 GRANT SELECT ON AutoProcIntegration TO ispyb_expeye_core_role;
+GRANT SELECT ON AutoProc TO ispyb_expeye_core_role;
 
 GRANT SELECT ON ProcessingJob TO ispyb_expeye_core_role;
 
