e21ce75bad6bfd3eee38a524bb892ad38a2886ea
Guilherme Francisco <guilherme.de-freitas@diamond.ac.uk>
2026-04-28 11:43:21 +0100

    Add comment clarifying constraint

--- schemas/ispyb/updates/2026_04_24_AutoProc_autoProcProgramId.sql
+++ schemas/ispyb/updates/2026_04_24_AutoProc_autoProcProgramId.sql
@@ -1,8 +1,13 @@
 INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2026_04_24_AutoProc_autoProcProgramId.sql', 'ONGOING');
 
+/* 
+ * Run SELECT * FROM AutoProc ap LEFT JOIN AutoProcProgram app ON ap.autoProcProgramId = app.autoProcProgramId WHERE app.autoProcProgramId is NULL;
+ * to list entries which don't match the constraint
+ */
+
 ALTER TABLE AutoProc
 ADD CONSTRAINT AutoProc_fk_autoProcProgramId
 FOREIGN KEY (autoProcProgramId)
-    REFERENCES AutoProc (autoProcProgramId);
+    REFERENCES AutoProcProgram (autoProcProgramId);
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2026_04_24_AutoProc_autoProcProgramId.sql';
