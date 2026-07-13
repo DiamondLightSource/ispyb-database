e3b66299e76608052d42d77bc0b909e448d94a60
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2026-07-13 10:57:20 +0100

    Update new experiment types

--- schemas/ispyb/updates/2026_07_02_ExperimentType_lamellaTomography.sql
+++ schemas/ispyb/updates/2026_07_02_ExperimentType_lamellaTomography.sql
@@ -1,6 +1,7 @@
 INSERT IGNORE INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2026_07_02_ExperimentType_lamellaTomography.sql', 'ONGOING');
 
-INSERT INTO ispyb.ExperimentType (experimentTypeId, name, proposalType, active) VALUES(48, 'Lamella Tomography', 'em', 1);
+INSERT INTO ExperimentType (experimentTypeId, name, proposalType, active) VALUES(48, 'Lamella Tomography', 'em', 1);
+INSERT INTO ExperimentType (experimentTypeId, name,proposalType,active) VALUES (49, 'Characterisation','mx',1);
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2026_07_02_ExperimentType_lamellaTomography.sql';
 
